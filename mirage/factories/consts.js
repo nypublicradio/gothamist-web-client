f104ef7d3c2c8d769348fcc4227136a4e6cc6fbe
Brian Whitton <me@brianwhitton.com>
2019-02-21 17:23:41 -0500

    adds a factory with traits for gothamist stories

---
+++ mirage/factories/consts.js
@@ -0,0 +1,46 @@
+export const DATE_FORMAT = 'YYYYMMDDhhmmss';
+
+export const PLATYPUS_GALLERY = {
+  has_gallery: true,
+  gallery_dropbox: true,
+  gallery_position: 'top_gallery_template',
+  gallery_array: ["http://gothamist.com/assets_c/2019/01/2019_01_1549-thumb-60x60-1026812.jpg"],
+  entrytopics: ["5ca7c4b6f9294fbf85ec17a6385c6a79"],
+  gallery_beforeafter: false,
+};
+
+export const MT_GALLERY = {
+  has_gallery: true,
+  gallery_dropbox: false,
+  gallery_position: 'top_gallery_template',
+  gallery_array: [
+    "http://gothamist.com/assets_c/2018/12/2018_12_snljm_mono-thumb-60x60-1026206.jpg",
+    "http://gothamist.com/assets_c/2018/12/2018_12_snljm_coldopen-thumb-60x60-1026210.jpg",
+    "http://gothamist.com/assets_c/2018/12/2018_12_snljm_elf-thumb-60x60-1026207.jpg",
+    "http://gothamist.com/assets_c/2018/12/2018_12_snljm_bigboy-thumb-60x60-1026211.jpg",
+    "http://gothamist.com/assets_c/2018/12/2018_12_snljm_dothraki-thumb-60x60-1026209.jpg",
+    "http://gothamist.com/assets_c/2018/12/2018_12_snljm_themtrumps-thumb-60x60-1026205.jpg",
+    "http://gothamist.com/assets_c/2018/12/2018_12_snljm_aidy-thumb-60x60-1026208.jpg",
+  ],
+  gallery_captions: [
+    "Muscular monologue",
+    "Robert DeNiro and Alex Moffat as Robert Mueller and Eric Trump <br/>",
+    "One Elf on the Shelf is having some problems with his kid",
+    "Big Boy appliances!",
+    "Finally, Dothraki Public Access ",
+    "Them Trumps!",
+    "Think Stefon, but a middle school girl from the midwest.",
+  ],
+  gallery_credit: [],
+  gallery_full: [
+    "http://gothamist.com/upload/2018/12/2018_12_snljm_mono.jpg",
+    "http://gothamist.com/upload/2018/12/2018_12_snljm_coldopen.jpg",
+    "http://gothamist.com/upload/2018/12/2018_12_snljm_elf.jpg",
+    "http://gothamist.com/upload/2018/12/2018_12_snljm_bigboy.jpg",
+    "http://gothamist.com/upload/2018/12/2018_12_snljm_dothraki.jpg",
+    "http://gothamist.com/upload/2018/12/2018_12_snljm_themtrumps.jpg",
+    "http://gothamist.com/upload/2018/12/2018_12_snljm_aidy.jpg",
+  ],
+  entrytopics: [],
+  gallery_beforeafter: false,
+};
---
+++ mirage/factories/story.js
@@ -0,0 +1,90 @@
+import moment from 'moment';
+import { Factory, faker, trait } from 'ember-cli-mirage';
+
+import {
+  DATE_FORMAT,
+  PLATYPUS_GALLERY,
+  MT_GALLERY,
+} from './consts';
+
+export default Factory.extend({
+  allow_comments: true,
+  author_id: () => faker.random.number({min: 500000, max: 550000}),
+  author_name: () => faker.internet.userName(),
+  author_nickname: () => faker.name.findName(),
+  authored_on: () => moment(faker.date.recent()).format(DATE_FORMAT),
+  authored_on_utc() {
+    return moment(this.authored_on, DATE_FORMAT).utc().format(DATE_FORMAT);
+  },
+  blog_id: 1,
+  categories: faker.list.random([{
+    basename: 'news',
+    label: 'News',
+  }, {
+    basename: 'arts',
+    label: 'Arts & Entertainment',
+  }, {
+    basename: 'food',
+    label: 'Food',
+
+  }]),
+  comment_count: 0,
+  entrytopics: () => [],
+  excerpt_full: () => faker.lorem.paragraph(7),
+  excerpt() {
+    return this.excerpt_full.slice(0, 220) + '...';
+  },
+  excerpt_pretty: () => faker.lorem.sentence(),
+  excerpt_sponsor: null,
+  has_gallery: false,
+  has_map: false,
+  modified_on() {
+    return moment(this.authored_on, DATE_FORMAT).add(2, 'h').format(DATE_FORMAT);
+  },
+  modified_on_utc() {
+    return moment(this.authored_on_utc, DATE_FORMAT).add(2, 'h').format(DATE_FORMAT);
+  },
+  national_title: null,
+  permalink() {
+    let slug = faker.lorem.words(3).split(' ').join('_')
+    let { authored_on:date } = this;
+    date = moment(date, DATE_FORMAT);
+    return `${date.format('YYYY')}/${date.format('MM')}/${date.format('DD')}/${slug}.php`;
+  },
+  platypus_id: () => faker.random.uuid(),
+  socialtopics: () => [],
+  tags: () => [],
+
+  text: () => faker.lorem.paragraphs(5),
+  text_more: null,
+
+  thumbnail_60: () => faker.image.imageUrl(60, 60, 'food', true, true),
+  thumbnail_105: () => faker.image.imageUrl(105, 105, 'food', true, true),
+  thumbnail_300: () => faker.image.imageUrl(300, 300, 'food', true, true),
+  thumbnail_640: () => faker.image.imageUrl(640, 480, 'food', true, true),
+  title: () => faker.random.words(6),
+
+  // TRAITS
+
+  sponsored: trait({
+    author_name: 'Sponsor',
+    author_nickname: 'Sponsor',
+    excerpt: () => faker.lorem.sentence() + ' [sponsor]',
+    excerpt_full: () => faker.lorem.sentence() + ' [sponsor]',
+    excerpt_sponsor: "<div id=\"sponsor-left\"><div class=\"sponsor-title\"><a href=\"http://gothamist.com/2019/02/05/escape_to_a_winter_happy_hour_at_ea_1.php\">Escape To A Winter Happy Hour At Eataly's SERRA ALPINA On The Roof</a></div><div class=\"sponsor-content\">\r\n<br><br><em>This post is brought to you by our sponsor, Eataly.</em></div></div><div id=\"sponsor-right\"><a href=\"http://gothamist.com/2019/02/05/escape_to_a_winter_happy_hour_at_ea_1.php\"><img src=\"http://gothamist.com/attachments/mei/Gothamist_Serra-Alpina_640x250_spon.jpg\"></a></div>",
+    tags: [
+      '@sponsor',
+    ]
+  }),
+
+  mtGallery: trait(MT_GALLERY),
+
+  platypusGallery: trait(PLATYPUS_GALLERY),
+
+  wrappedGallery: trait({
+    ...PLATYPUS_GALLERY,
+    gallery_position: 'default_gallery_template',
+    text: () => faker.lorem.paragraphs(1),
+    text_more: () => faker.lorem.paragraphs(5),
+  }),
+});
