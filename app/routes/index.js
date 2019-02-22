be119b052ebeb17e73f1e272fbc2b29c56b8c0e8
Brian Whitton <me@brianwhitton.com>
2019-02-22 13:08:51 -0500

    replace raw fetch call on homepage with the ember data store

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,11 @@
 import Route from '@ember/routing/route';
-import fetch from 'fetch';
 
 export default Route.extend({
   model() {
-    return fetch('https://api.demo.nypr.digital/topics/search?index=gothamist&term=@main&count=3')
-      .then(r => r.json())
-      .then(({entries}) => entries);
+    return this.store.query('story', {
+      index: 'gothamist',
+      term: '@main',
+      count: 3
+    });
   }
 });
