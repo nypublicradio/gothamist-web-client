1f69b5885a49adfbf62a353c3e07c42a0e3efad2
Brian Whitton <me@brianwhitton.com>
2019-02-22 13:14:38 -0500

    tweak how queryRecord responses are handled
    pull out the first entry in the array b/c ember expects the value to be 
    a single object, not a list

--- app/serializers/story.js
+++ app/serializers/story.js
@@ -4,4 +4,11 @@ import { underscore } from '@ember/string';
 export default DS.RESTSerializer.extend({
   modelNameFromPayloadKey: () => 'story',
   keyForAttribute: key => underscore(key),
+
+  normalizeQueryRecordResponse(store, storyClass, payload) {
+    // GothTopics always returns an array of entries
+    // ember wants a single record in response to `queryRecord` calls
+    payload.entries = payload.entries[0];
+    return this._super(...arguments);
+  }
 });
