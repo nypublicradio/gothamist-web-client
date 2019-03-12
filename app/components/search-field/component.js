05ff325d87027ff04bb22c9b1365be4e2004f163
Brian Whitton <me@brianwhitton.com>
2019-03-12 11:29:34 -0400

    update search behavior
    if there are 3 or more articles that match a given search term, go to 
    the tag listen for that term, otherwise do a google site search

--- app/components/search-field/component.js
+++ app/components/search-field/component.js
@@ -3,8 +3,24 @@ import { inject } from '@ember/service';
 
 export default Component.extend({
   router: inject(),
+  store:  inject(),
 
   search(value) {
+    // if there are 3 or more articles for the given query
+    // go to the tag listing for that term
+    // otherwise do a full site search
+    this.store.query('story', {
+      index: 'gothamist',
+      offset: 2, // start at 3
+      count: 1, // only  need to know if there's at least 1 entry (i.e. 3 or more is valid)
+      term: value,
+    })
+    .then(result => {
+      if (result.meta.count > 0) {
+        this.router.transitionTo('tags', value);
+      } else {
         this.router.transitionTo('search', {queryParams: {q: value}});
+      }
+    });
   }
 });
