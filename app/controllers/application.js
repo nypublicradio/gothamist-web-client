e01e68fb06a2890e6d4c5994cc5ac786290e332a
Brian Whitton <me@brianwhitton.com>
2019-04-11 14:36:59 -0400

    pass in local search function

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -22,4 +22,30 @@ export default Controller.extend({
   headline: null,
 
   headerLandmark: null,
+
+  /**
+   Conduct a search
+
+   @method search
+   @param {String} q The search query
+   @return {void}
+  */
+  search(value) {
+    // if there are 3 or more articles for the given query
+    // go to the tag listing for that term
+    // otherwise do a full site search
+    this.store.query('story', {
+      index: 'gothamist',
+      offset: 2, // start at 3
+      count: 1, // only  need to know if there's at least 1 entry (i.e. 3 or more is valid)
+      term: value,
+    })
+    .then(result => {
+      if (result.meta.count > 0) {
+        this.router.transitionTo('tags', value);
+      } else {
+        this.router.transitionTo('search', {queryParams: {q: value}});
+      }
+    });
+  }
 });
