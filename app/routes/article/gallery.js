2427051e52827b02f4e77bb949690954f15407e6
Brian Whitton <me@brianwhitton.com>
2019-04-17 23:27:01 -0400

    support for server-side OG images
    update a query param as the user scrolls and check it on requests. 
    update the og image with teh image the at the given index if a query 
    param exists.

--- app/routes/article/gallery.js
+++ app/routes/article/gallery.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 export default Route.extend({
+  fastboot: inject(),
+  headData: inject(),
+
+  // isFastBoot: reads('fastboot.isFastBoot'),
+  image: reads('fastboot.request.queryParams.image'),
 
   titleToken: model => `Photos - ${model.title}`,
 
@@ -12,5 +19,13 @@ export default Route.extend({
       showLeaderboard: false,
       headerLandmark: null,
     });
+
+    if (this.image) {
+      let slide = model.gallery.slides[this.image];
+      let src = slide ? slide.full : '';
+      this.headData.setProperties({
+        metaImage: src
+      });
+    }
   }
 });
