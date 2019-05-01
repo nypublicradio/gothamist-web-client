f281431b70c3829626d5afa8438296a86df80e2d
Brian Whitton <me@brianwhitton.com>
2019-05-01 15:18:03 -0400

    add test cases for sponsored posts

--- tests/acceptance/homepage-test.js
+++ tests/acceptance/homepage-test.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 import { module, test } from 'qunit';
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
@@ -32,5 +34,28 @@ module('Acceptance | homepage', function(hooks) {
     await click('[data-test-more-results]');
 
     assert.dom('[data-test-block]').exists({count: (TOTAL_COUNT * 2) + 1}, 'Clicking "read more" brings in another set of results equal to the amount of TOTAL_COUNT');
+
+  test('sponsored posts younger than 24 hours appear in sponsored tout', async function(assert) {
+    const TITLE = 'foo';
+
+    server.create('article', {
+      title: TITLE,
+      tags: ['@sponsor'],
+      authored_on: moment().subtract(12, 'hours'),
+    });
+
+    await visit('/');
+    assert.dom('[data-test-sponsored-tout] .c-block__title').hasText(TITLE);
   });
+
+  test('sponsored posts older than 24 hours do not appear in sponsored tout', async function(assert) {
+
+    server.create('article', {
+      tags: ['@sponsor'],
+      authored_on: moment().subtract(36, 'hours'),
+    });
+
+    await visit('/');
+    assert.dom('[data-test-sponsored-tout]').doesNotExist();
+  })
 });
