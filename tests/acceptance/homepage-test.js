373a27e57297224ccd66731010858f1ce7cae10c
Brian Whitton <me@brianwhitton.com>
2019-05-01 11:28:20 -0400

    homepage acceptance test

---
+++ tests/acceptance/homepage-test.js
@@ -0,0 +1,36 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+import { TOTAL_COUNT } from 'gothamist-web-client/routes/index';
+
+
+module('Acceptance | homepage', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('visiting homepage', async function(assert) {
+    server.createList('article', 10, {
+      tags: ['@main']
+    });
+    server.create('article', {tags: ['@sponsored']});
+    server.createList('article', TOTAL_COUNT * 2);
+
+    server.create('wnyc-story', {id: 'gothamist-wnyc-crossposting'});
+
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('[data-test-featured-block-list]').exists();
+    assert.dom('[data-test-featured-block-list] [data-test-block]').exists();
+    assert.dom('[data-test-block]').exists({count: TOTAL_COUNT + 1}, `${TOTAL_COUNT} blocks plus one for sponsored tout`);
+
+    const mainArticleInRiver = server.schema.articles.where({tags:['@main']}).slice(-1).models[0];
+    assert.dom(`[data-test-block="${mainArticleInRiver.id}"]`).doesNotHaveClass('c-block--horizontal', 'articles in the "river" tagged main should not have a "--horizontal" modifier class');
+
+    await click('[data-test-more-results]');
+
+    assert.dom('[data-test-block]').exists({count: (TOTAL_COUNT * 2) + 1}, 'Clicking "read more" brings in another set of results equal to the amount of TOTAL_COUNT');
+  });
+});
