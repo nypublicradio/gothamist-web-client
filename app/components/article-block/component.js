241e6b24fb20243135a4a5409a3d6c9601d53fc8
Brian Whitton <me@brianwhitton.com>
2019-05-02 11:01:59 -0400

    timestamp relative units

--- app/components/article-block/component.js
+++ app/components/article-block/component.js
@@ -3,16 +3,26 @@ import moment from 'moment';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 
+export const JUST_NOW = 'Just now';
+export const RECENT = 'Less than one hour ago';
+export const TIMESTAMP_FORMAT = 'MMM D, YYYY h:mm a';
+
 export default Component.extend({
   tagName: '',
 
   timestamp: computed('article', function() {
     let { publishedMoment:pub } = this.article;
-    let diff = moment().diff(pub, 'hours');
-    if (diff < 24) {
-      return `${diff} hours ago`;
+    let now = moment();
+    let minutes = now.diff(pub, 'minutes')
+    let hours = now.diff(pub, 'hours');
+    if (minutes <= 5) {
+      return JUST_NOW;
+    } else if (minutes > 5 && hours < 1) {
+      return RECENT;
+    } else if (hours < 24) {
+      return `${hours} hour${hours > 1 ? 's' : ''} ago`
     } else {
-      return pub.format('MMM D, YYYY h:mm a');
+      return pub.format(TIMESTAMP_FORMAT);
     }
   }),
 
