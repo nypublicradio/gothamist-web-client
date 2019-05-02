0412258e7586c5d2903dc5a1b1282f1dc2062f1d
Brian Whitton <me@brianwhitton.com>
2019-05-02 12:32:12 -0400

    initial tag page

--- app/routes/tags.js
+++ app/routes/tags.js
@@ -1,25 +1,32 @@
-import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
+
+import Route from '@ember/routing/route';
 import { classify } from '@ember/string';
+import { inject } from '@ember/service';
 
 const { hash } = RSVP;
 
+const titleize = ([f, ...rest]) => `${f.toUpperCase()}${rest.join('')}`;
+
 export default Route.extend({
+  header: inject('nypr-o-header'),
+
   titleToken: model => classify(model.tag),
 
-  queryParams: {
-    page: {
-      refreshModel: true,
-    }
+  beforeModel() {
+    this.header.addRule('tags', {
+      all: {
+        nav: true,
+        donate: true,
+        search: true,
+      },
+    });
   },
 
   model({ tag, page = 1 }) {
-    if (['news', 'arts-entertainment', 'food'].includes(tag)) {
-      tag = `c|${tag}`;
-    }
     return hash({
-      tag,
-      stories: this.store.query('article', {
+      tag: tag.replace(/(\w)\w+/g, titleize),
+      articles: this.store.query('article', {
         index: 'gothamist',
         term: tag,
         count: 10,
