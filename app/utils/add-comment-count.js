d3861317a0bf68acb533a58ab08bd0b4ca6ec30a
Brian Whitton <me@brianwhitton.com>
2019-05-03 15:47:10 -0400

    adds comment count util
    updates a given model or list of models with comment counts as fetched 
    from the disqus `thread/set` endpoint.

---
+++ app/utils/add-comment-count.js
@@ -0,0 +1,60 @@
+import DS from 'ember-data';
+import fetch from 'fetch';
+
+import config from '../config/environment';
+
+
+export const QUERY_PARAMS = {
+  api_key: config.disqusKey,
+  forum: 'gothamist',
+};
+export const BASE = `${config.disqusAPI}/threads/set.json`;
+
+/**
+  Takes the given model or list of models (aka RecordArray) and fetches their comment count
+  from the Disqus API.
+
+  Catches network errors and prints them to the console.
+
+  @function addCommentCount
+  @param modelOrRecordArray {DS.Model|DS.RecordArray}
+  @return {void}
+*/
+export default async function addCommentCount(modelOrRecordArray) {
+  let qp = Object.keys(QUERY_PARAMS).map(key => `${key}=${QUERY_PARAMS[key]}`);
+
+  if (modelOrRecordArray instanceof DS.Model) {
+    qp.push(`thread:ident=${modelOrRecordArray.id}`);
+  } else if (modelOrRecordArray instanceof DS.RecordArray) {
+    modelOrRecordArray.mapBy('id').forEach(id => qp.push(`thread:ident=${id}`));
+  }
+
+  let res;
+  try {
+    res = await fetch(`${BASE}?${qp.join('&')}`);
+    if (res.status !== 200) {
+      const e = new Error('Disqus API call failed.');
+      e.response = res;
+      throw e;
+    }
+  } catch(e) {
+    console.warn(e); // eslint-disable-line
+  }
+
+  let { response } = await res.json();
+
+  if (modelOrRecordArray instanceof DS.Model) {
+    let [thread] = response;
+    modelOrRecordArray.set('commentCount', thread.posts);
+  } else if (modelOrRecordArray instanceof DS.RecordArray){
+    response.forEach(thread => {
+      let { identifiers, posts } = thread;
+      let [ id ] = identifiers;
+      let article = modelOrRecordArray.findBy('id', id);
+      if (article) {
+        article.set('commentCount', posts);
+      }
+    });
+  }
+
+}
---
+++ tests/unit/utils/add-comment-count-test.js
@@ -0,0 +1,59 @@
+import * as fetch from 'fetch';
+
+import { module } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
+
+import addCommentCount, {
+  QUERY_PARAMS,
+  BASE,
+} from 'gothamist-web-client/utils/add-comment-count';
+
+module('Unit | Utility | addCommentCount', function(hooks) {
+  setupTest(hooks);
+
+  test('it updates an article model', async function(assert) {
+    const EXPECTED = 100;
+    let store = this.owner.lookup('service:store');
+    const ARTICLE = store.createRecord('article', {id: 'foo'});
+
+    let qp = {...QUERY_PARAMS, ...{'thread:ident': ARTICLE.id}};
+    qp = Object.keys(qp).map(k => `${k}=${qp[k]}`);
+    this.mock(fetch)
+      .expects('default')
+      .withArgs(`${BASE}?${qp.join('&')}`)
+      .resolves(new Response(JSON.stringify({response: [{posts: EXPECTED}]})));
+
+    await addCommentCount(ARTICLE);
+
+    assert.equal(ARTICLE.commentCount, EXPECTED);
+  });
+
+  test('it updates a record array', async function(assert) {
+    const EXPECTED = [100, 200, 300];
+    let store = this.owner.lookup('service:store');
+    let qp = Object.keys(QUERY_PARAMS).map(k => `${k}=${QUERY_PARAMS[k]}`);
+
+    for (let id = 0; id < 3; id++) {
+      store.createRecord('article', {id});
+      qp.push(`thread:ident=${id}`);
+    }
+
+    this.mock(fetch)
+      .expects('default')
+      .withArgs(`${BASE}?${qp.join('&')}`)
+      .resolves(new Response(JSON.stringify({
+        response: EXPECTED.map((posts, id) => ({
+          posts,
+          identifiers: [String(id)]
+        })
+      )})));
+
+    await addCommentCount(store.peekAll('article'));
+
+    store.peekAll('article').forEach((article, i) => {
+      assert.equal(article.commentCount, EXPECTED[i]);
+    });
+
+  });
+});
