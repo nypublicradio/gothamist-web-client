6d32431f03232940b41230e06cbfa7e7bf75db49
Brian Whitton <me@brianwhitton.com>
2019-05-03 16:59:51 -0400

    add cimment count on article route

--- app/routes/article/index.js
+++ app/routes/article/index.js
@@ -1,12 +1,10 @@
 /* global instgrm */
-import fetch from 'fetch';
-
 import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 import { reads } from '@ember/object/computed';
 import { schedule } from '@ember/runloop';
 
-import config from '../../config/environment';
+import addCommentCount from '../../utils/add-comment-count';
 
 export default Route.extend({
   fastboot: inject(),
@@ -38,22 +36,9 @@ export default Route.extend({
     // save the comment API call for the client
     if (this.isFastBoot) {
       return;
+    } else if (model.allowComments){
+      addCommentCount(model);
     }
-
-    // fetch comments count
-    fetch(`${config.disqusAPI}/threads/details.json?thread:ident=${model.id}&api_key=${config.disqusKey}&forum=gothamist`)
-      .then(r => {
-        if (r.status !== 200) {
-          const e = new Error("Disqus API call failed.");
-          e.response = r;
-          throw e;
-        } else {
-          return r;
-        }
-      })
-      .then(r => r.json())
-      .then(({ response }) => model.set('commentCount', response.posts))
-      .catch(e => console.warn(e.message)); // eslint-disable-line
 
     schedule('afterRender', () => {
       // instagram embeds need a manual push after rehydration
--- tests/acceptance/article-test.js
+++ tests/acceptance/article-test.js
@@ -3,6 +3,8 @@ import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
+import config from 'gothamist-web-client/config/environment';
+
 module('Acceptance | article', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
@@ -24,6 +26,21 @@ module('Acceptance | article', function(hooks) {
     assert.dom('[data-test-top-nav]').exists('nav should exist at load');
     assert.dom('[data-test-article-headline]').hasText(article.title);
     assert.dom('[data-test-article-body]').hasText(article.text);
+  });
+
+  test('comment counts', async function(assert) {
+    const ARTICLE = server.create('article');
+    const EXPECTED = 100;
+    server.get(`${config.disqusAPI}/threads/set.json`, {
+      response: [{
+        posts: EXPECTED, identifiers: [ARTICLE.id]
+      }]
+    });
+
+    await visit(`/${ARTICLE.permalink}`);
+
+    assert.dom('.c-article__meta-group a').hasText(`${EXPECTED} Comments`);
+    assert.dom('#comments').exists({count: 1});
   });
 
   test('no disqus if comments are not allowed', async function(assert) {
