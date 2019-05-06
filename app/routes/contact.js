6577af04b9c74164d435c5c4dd2e0ecd4dd6e262
Brian Whitton <me@brianwhitton.com>
2019-05-06 19:16:58 -0400

    contact page

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
 
 export default Route.extend({
+  header: inject('nypr-o-header'),
+
+  titleToken: 'Contact Us',
+
+  beforeModel() {
+    this.header.addRule('staff.index', {
+      all: {
+        nav: true,
+        donate: true,
+        search: true,
+      }
+    });
+  },
+
 });
