a627811b6cc7a0ef73279dc6104d79d27389b72d
Brian Whitton <me@brianwhitton.com>
2019-05-06 12:56:07 -0400

    header rules

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,5 +1,18 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
 
 export default Route.extend({
-  titleToken: 'Search Results'
+  header: inject('nypr-o-header'),
+
+  titleToken: 'Search Results',
+
+  beforeModel() {
+    this.header.addRule('search', {
+      all: {
+        nav: true,
+        donate: true,
+        search: true,
+      }
+    });
+  }
 });
