1b81dfc1f37ed9cece3972c9ec36b891069cce2c
Brian Whitton <me@brianwhitton.com>
2019-05-13 18:31:58 -0400

    basic mirage serialization to include meta info

--- mirage/serializers/article.js
+++ mirage/serializers/article.js
@@ -4,4 +4,14 @@ import { underscore } from '@ember/string';
 export default Serializer.extend({
   keyForCollection: () => 'entries',
   keyForAttribute: attr => underscore(attr),
+
+  serialize(object, request) {
+    let json = Serializer.prototype.serialize.apply(this, arguments);
+
+    return {
+      ...json,
+      total_entries: object.models.length,
+      listed_entries: parseInt(request.queryParams.count, 10),
+    };
+  }
 });
