f09f08856f39d0c252b0ff549c52539b2b37e5c9
Brian Whitton <me@brianwhitton.com>
2019-05-13 18:27:40 -0400

    adds `isFinished` property

--- app/components/query-more/component.js
+++ app/components/query-more/component.js
@@ -1,7 +1,11 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
+import { computed, get } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import { task } from 'ember-concurrency';
+
+
+const DEFAULT_TOTAL_KEY = 'total';
 
 export default Component.extend({
   store: inject(),
@@ -11,7 +15,19 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     this.set('pages', []);
+
+    if (!this.totalKey) {
+      this.set('totalKey', DEFAULT_TOTAL_KEY);
+    }
   },
+
+  /**
+    Key to use to look up total result size on the store response's `meta` object.
+
+    @argumeemnt totalKey
+    @type {String}
+    @defult 'count'
+  */
 
   /**
     Model name to query for in the store
@@ -36,15 +52,24 @@ export default Component.extend({
   */
 
   /**
-    Runs a query on the story using the arguments passed in at run time. returns a two dimensional array of pages.
+    Stored total count from server query. Used to calculate `isFinished`
+
+    @field total
+    @type {Number}
+  */
+
+  /**
+    Runs a query on the store using the arguments passed in at run time. returns a two dimensional array of pages.
 
     **NOTE** This is an ember concurrency task, so you need to use the `perform` helper instad of
     the `action` helper when attaching it as a click handler.
     @method queryMore
-    @return {any} pages
+    @return {Array[Model]} pages
   */
   queryMore: task(function *() {
     let results = yield this.store.query(this.model, this.query);
+
+    this.set('total', get(results, `meta.${this.totalKey}`));
 
     if (this.page) {
       this.incrementProperty('page');
@@ -65,4 +90,22 @@ export default Component.extend({
     @type {Number}
   */
   page: alias('query.page'),
+
+  /**
+    Reflects if there are more results on the server.
+
+    @accessor isFinished
+    @type {Boolean}
+  */
+  isFinished: computed('pages.[]', 'total', 'query.count', function() {
+    let pageSize = this.query && this.query.count;
+    if (!pageSize) {
+      console.warn('`count` must be provided to the query in order to calculate `hasMore`'); // eslint-disable-line
+      return;
+    }
+
+    let retrievedResults = this.pages.length * pageSize;
+    // is there at least one more page remaining on the server?
+    return retrievedResults + pageSize > this.total;
+  }),
 });
