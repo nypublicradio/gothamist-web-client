e4ae547a8e0cd7b5e578c7d8c421b144baf1c55a
Brian Whitton <me@brianwhitton.com>
2019-05-13 18:32:15 -0400

    adds staff detail page

--- app/routes/staff/detail.js
+++ app/routes/staff/detail.js
@@ -1,4 +1,62 @@
+import RSVP from 'rsvp';
+
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+
+import fade from 'ember-animated/transitions/fade';
+
+import addCommentCount from '../../utils/add-comment-count';
+
+
+const { hash } = RSVP;
+export const COUNT = 12;
 
 export default Route.extend({
+  fastboot: inject(),
+  header: inject('nypr-o-header'),
+
+  titleToken: model => model.namee,
+
+  beforeModel() {
+    this.header.addRule('staff.detail', {
+      all: {
+        nav: true,
+        donate: true,
+        search: true,
+      }
+    });
+  },
+
+  model({ name }) {
+    return hash({
+      name,
+      articles: this.store.query('article', {
+        index: 'gothamist',
+        term: `a|${name}`,
+        count: COUNT,
+      })
+    })
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+
+    controller.setProperties({
+      query: {
+        index: 'gothamist',
+        term: model.name,
+        count: COUNT,
+        page: 2,
+      },
+      transition: fade,
+    });
+
+    if (this.fastboot.isFastBoot) {
+      return;
+    } else {
+      addCommentCount(model.articlees);
+
+      controller.set('addComments', results => (addCommentCount(results), results));
+    }
+  }
 });
--- tests/unit/routes/staff/index-test.js
+++ tests/unit/routes/staff/index-test.js
@@ -5,7 +5,7 @@ module('Unit | Route | staff', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:staff');
+    let route = this.owner.lookup('route:staff.index');
     assert.ok(route);
   });
 });
