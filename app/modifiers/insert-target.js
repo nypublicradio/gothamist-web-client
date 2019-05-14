87446d649a1e90518d113399ba275efaba04a35d
walsh9 <walsh9@gmail.com>
2019-05-14 11:25:34 -0400

    Lets call these nodes

--- app/modifiers/insert-target.js
+++ app/modifiers/insert-target.js
@@ -11,23 +11,23 @@ const dontInsertAfter = ['h1', 'h2', 'h3', 'h4', 'h5', ...embeds];
 
 export function insertTarget(element, [id], {wordBoundary=300, containerSelector}) {
   let container = document.querySelector(containerSelector) || element;
-  let children = [...container.childNodes].filter(node => {
+  let nodes = [...container.childNodes].filter(node => {
     // ignore whitespace only text and P nodes.
     return !(['#text', 'P'].includes(node.nodeName)
       && node.textContent.replace(/\s/g, '').length === 0);
   });
   let wordCount = 0;
-  let boundary = children.find((child, index) => {
-    wordCount += countWords(child);
+  let boundary = nodes.find((node, index) => {
+    wordCount += countWords(node);
 
-    let currentTag = child.nodeName.toLowerCase();
-    let nextChild = children[index+1];
-    let nextTag =  nextChild && nextChild.nodeName.toLowerCase();
+    let currentTag = node.nodeName.toLowerCase();
+    let nextNode = nodes[index+1];
+    let nextTag =  nextNode && nextNode.nodeName.toLowerCase();
 
     if (wordCount >= wordBoundary
       && !dontInsertAfter.includes(currentTag)
       && !dontInsertBefore.includes(nextTag)) {
-      return child;
+      return node;
     }
   })
   if (boundary) {
