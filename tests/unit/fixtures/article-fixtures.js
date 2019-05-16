dcdb95f0ff49fdab40e1ba43a223405e0d020c1c
Brian Whitton <me@brianwhitton.com>
2019-05-15 22:03:37 -0400

    move these into a separate module

---
+++ tests/unit/fixtures/article-fixtures.js
@@ -0,0 +1,47 @@
+export const CAPTION_WITH_CREDIT = `
+<p>
+  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
+    <div class="image-none">
+      <img alt="bigboy040819.jpeg" src="http://gothamist.com/attachments/nyc_clampen/bigboy040819.jpeg" width="640" height="480" />
+      <br />
+      <i> Barsik may just be NYC's biggest cat. (Animal Care Centers of NYC)</i>
+    </div>
+  </span>
+</p>
+`;
+
+export const CAPTION_WITH_WHITESPACE = `
+<p>
+  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
+    <div class="image-none">
+      <img alt="bigboy040819.jpeg" src="http://gothamist.com/attachments/nyc_clampen/bigboy040819.jpeg" width="640" height="480" />
+      <br />
+      <i> Joseph Jordan a.k.a Eric Striker (Courtesy of the Southern Poverty Law Center) </i>
+    </div>
+  </span>
+</p>
+`;
+
+export const CREDIT_WITH_LINK = `
+<p>
+  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
+    <div class="image-none">
+      <img alt="bigboy040819.jpeg" src="http://gothamist.com/attachments/nyc_clampen/bigboy040819.jpeg" width="640" height="480" />
+      <br />
+      <i> Barsik may just be NYC's biggest cat. (<a href="http://example.com">Animal Care Centers of NYC</a>)</i>
+    </div>
+  </span>
+</p>
+`;
+
+export const CAPTION_WITH_LINK = `
+<p>
+  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
+    <div class="image-none">
+      <img alt="050819latch1.jpg" src="http://gothamist.com/attachments/nyc_arts_john/050819latch1.jpg" width="640" height="473" />
+      <br />
+      <i> A Latch M-series keyless entrance, <a href="https://www.latch.com/m-series">via Latch's website</a>.</i>
+    </div>
+  </span>
+</p>
+`;
--- tests/unit/models/article-test.js
+++ tests/unit/models/article-test.js
@@ -1,54 +1,14 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
+import {
+  CAPTION_WITH_CREDIT,
+  CREDIT_WITH_LINK,
+  CAPTION_WITH_LINK,
+  CAPTION_WITH_WHITESPACE,
+  CAPTION_WITH_MULTIPLE_PARENS,
+} from '../fixtures/article-fixtures';
 
-const CAPTION_WITH_CREDIT = `
-<p>
-  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
-    <div class="image-none">
-      <img alt="bigboy040819.jpeg" src="http://gothamist.com/attachments/nyc_clampen/bigboy040819.jpeg" width="640" height="480" />
-      <br />
-      <i> Barsik may just be NYC's biggest cat. (Animal Care Centers of NYC)</i>
-    </div>
-  </span>
-</p>
-`;
-
-const CAPTION_WITH_WHITESPACE = `
-<p>
-  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
-    <div class="image-none">
-      <img alt="bigboy040819.jpeg" src="http://gothamist.com/attachments/nyc_clampen/bigboy040819.jpeg" width="640" height="480" />
-      <br />
-      <i> Joseph Jordan a.k.a Eric Striker (Courtesy of the Southern Poverty Law Center) </i>
-    </div>
-  </span>
-</p>
-`;
-
-const CREDIT_WITH_LINK = `
-<p>
-  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
-    <div class="image-none">
-      <img alt="bigboy040819.jpeg" src="http://gothamist.com/attachments/nyc_clampen/bigboy040819.jpeg" width="640" height="480" />
-      <br />
-      <i> Barsik may just be NYC's biggest cat. (<a href="http://example.com">Animal Care Centers of NYC</a>)</i>
-    </div>
-  </span>
-</p>
-`;
-
-const CAPTION_WITH_LINK = `
-<p>
-  <span class="mt-enclosure mt-enclosure-image" style="display: inline;">
-    <div class="image-none">
-      <img alt="050819latch1.jpg" src="http://gothamist.com/attachments/nyc_arts_john/050819latch1.jpg" width="640" height="473" />
-      <br />
-      <i> A Latch M-series keyless entrance, <a href="https://www.latch.com/m-series">via Latch's website</a>.</i>
-    </div>
-  </span>
-</p>
-`;
 
 module('Unit | Model | article', function(hooks) {
   setupTest(hooks);
