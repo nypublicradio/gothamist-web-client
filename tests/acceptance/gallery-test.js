8db08008b12212254e9f3ab86e68447f477ae91c
walsh9 <walsh9@gmail.com>
2019-05-21 16:58:53 -0400

    Update gallery ad rules and add tests

---
+++ tests/acceptance/gallery-test.js
@@ -0,0 +1,88 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | gallery', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(() => {
+    window.block_disqus = true;
+  });
+
+  hooks.afterEach(() => {
+    window.block_disqus = false;
+  })
+
+  test('gallery should contain 2 slides and 0 ads', async function(assert) {
+    const article = server.create('article', 'mtGallery', {categories: [{basename: 'food'}]});
+    // 7 articles - 5 = 2
+    article.gallery_array.splice(0, 5)
+    article.gallery_captions.splice(0, 5)
+    article.gallery_full.splice(0, 5)
+
+    await visit(`/${article.permalink}/gallery`);
+
+    assert.equal(currentURL(), `/${article.permalink}/gallery`);
+    assert.dom('[data-test-gallery-slide]').exists({count: 2});
+    assert.dom('[data-test-gallery-overlay] [data-test-ad-tag-wide]').exists({count: 0});
+  });
+
+  test('gallery should contain 3 slides and 1 ad', async function(assert) {
+    const article = server.create('article', 'mtGallery', {categories: [{basename: 'food'}]});
+    // 7 articles - 4 = 3
+    article.gallery_array.splice(0, 4);
+    article.gallery_captions.splice(0, 4);
+    article.gallery_full.splice(0, 4);
+
+    await visit(`/${article.permalink}/gallery`);
+
+    assert.equal(currentURL(), `/${article.permalink}/gallery`);
+    assert.dom('[data-test-gallery-slide]').exists({count: 3});
+    assert.dom('[data-test-gallery-overlay] [data-test-ad-tag-wide]').exists({count: 1});
+  });
+
+  test('gallery should contain 5 slides and 1 ads', async function(assert) {
+    const article = server.create('article', 'mtGallery', {categories: [{basename: 'food'}]});
+    // 7 articles - 2 = 6
+    article.gallery_array.splice(0, 2)
+    article.gallery_captions.splice(0, 2)
+    article.gallery_full.splice(0, 2)
+
+    await visit(`/${article.permalink}/gallery`);
+
+    assert.equal(currentURL(), `/${article.permalink}/gallery`);
+    assert.dom('[data-test-gallery-slide]').exists({count: 5});
+    assert.dom('[data-test-gallery-overlay] [data-test-ad-tag-wide]').exists({count: 1});
+  });
+
+
+  test('gallery should contain 6 slides and 2 ads', async function(assert) {
+    const article = server.create('article', 'mtGallery', {categories: [{basename: 'food'}]});
+    // 7 articles - 1 = 6
+    article.gallery_array.splice(0, 1)
+    article.gallery_captions.splice(0, 1)
+    article.gallery_full.splice(0, 1)
+
+    await visit(`/${article.permalink}/gallery`);
+
+    assert.equal(currentURL(), `/${article.permalink}/gallery`);
+    assert.dom('[data-test-gallery-slide]').exists({count: 6});
+    assert.dom('[data-test-gallery-overlay] [data-test-ad-tag-wide]').exists({count: 2});
+  });
+
+  test('gallery should contain 14 slides and still only 2 ads', async function(assert) {
+    const article = server.create('article', 'mtGallery', {categories: [{basename: 'food'}]});
+    // 7 articles + 7 = 14
+    article.gallery_array.push(...article.gallery_array);
+    article.gallery_captions.push(...article.gallery_captions);
+    article.gallery_full.push(...article.gallery_full);
+
+    await visit(`/${article.permalink}/gallery`);
+
+    assert.equal(currentURL(), `/${article.permalink}/gallery`);
+    assert.dom('[data-test-gallery-slide]').exists({count: 14});
+    assert.dom('[data-test-gallery-overlay] [data-test-ad-tag-wide]').exists({count: 2});
+  });
+});
