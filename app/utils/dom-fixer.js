0fd6a906028c5724986dfa40cbb33f363d46698c
Brian Whitton <bwhitton@wnyc.org>
2019-05-23 12:26:41 -0400

    adds a DomFixer class to manage common DOM repairs
    trying to encapsulate most of the logic tht was in the article model

---
+++ app/utils/dom-fixer.js
@@ -0,0 +1,226 @@
+/**
+  Encapsulates some common DOM repairs
+
+  @class DomFixer
+  @param rawText {String}
+  @export default
+*/
+export default class DomFixer {
+  constructor(rawText) {
+    if (typeof rawText === 'undefined') {
+      throw new Error("Must provide a string when initializing");
+    }
+    const range = document.createRange();
+    const nodes = range.createContextualFragment(rawText);
+
+    this.nodes = nodes;
+  }
+
+  querySelector(selector) {
+    return this.nodes.querySelector(selector);
+  }
+
+  querySelectorAll(selector) {
+    return this.nodes.querySelectorAll(selector);
+  }
+
+  /**
+    Removes all the empty `#text` nodes found within the DOM tree
+
+    @method removeEmptyNodes
+    @return void
+  */
+  removeEmptyNodes() {
+    const whiteWalker = getWhiteWalker(this.nodes);
+    const empties = [];
+
+    while(whiteWalker.nextNode()) {
+      empties.push(whiteWalker.currentNode);
+    }
+
+    empties.forEach(n => n.parentNode.removeChild(n));
+
+    this.emptied = true;
+  }
+
+  /**
+    Finds all the `#text` nodes that are direct children of the given `root` selector. Gathers adjacent nodes and wraps them under a new `<p/>` tag and inserts it into the tree.
+
+    @method rescueOrphans
+    @param root {Node|String} `root` can be a `Node` or a `String`. If it's a string, this will be used as a `querySelectorAll` argument, and the method will run recursively on the result set.
+    @param options {Object} `orphanRoot` tag name to serve as a wrapping element for gathered `#text` nodes.
+    @return {void}
+  */
+  rescueOrphans(root = this.nodes, options = {}) {
+    if (typeof root === 'string') {
+      this.querySelectorAll(root).forEach(this.rescueOrphans.bind(this));
+      return;
+    }
+
+    if (typeof root === 'object' && arguments.length === 1) {
+      options = root;
+      root = this.nodes;
+    }
+    options = {...options, ...DEFAULT_ORPHAN_OPTIONS};
+    const orphanWalker = getOrphanWalker(root);
+
+    while(orphanWalker.nextNode()) {
+      let node = orphanWalker.currentNode;
+      const adjacentOrphans = [];
+
+      while(node && ORPHAN_NODES.includes(node.nodeName)) {
+        adjacentOrphans.push(node);
+        node = node.nextSibling;
+      }
+
+      const orphanRoot = document.createElement(options.orphanRoot);
+      // get sibling of final orphan as an insert target
+      const target = adjacentOrphans.slice(-1)[0].nextSibling;
+
+      adjacentOrphans.forEach(orphan => orphanRoot.appendChild(orphan));
+      root.insertBefore(orphanRoot, target);
+    }
+  }
+
+  /**
+    Finds all paragraphs that contain adjacent line breaks (`<br/>`) and splits it into two paragraphs. Double line breaks are intended to be a new paragraph.
+
+    @method unbreakParagraphs
+    @param root {Node} Root node at which to start looking for paragraphs containing double line breaks
+    @return {void}
+  */
+  unbreakParagraphs(root = this.nodes) {
+    if (!this.emptied) {
+      throw new Error('Empty nodes must be removed before paragraphs can be split.');
+    }
+
+    const brokenGrafWalker = getBrokenWalker(root);
+
+    while(brokenGrafWalker.nextNode()) {
+      const graf = brokenGrafWalker.currentNode;
+      const adjacentBreaks = [
+        ...graf.querySelectorAll('br + br')
+      ].map(br => ([br.previousSibling, br]));
+
+      // for each break boundary
+      adjacentBreaks.forEach(([break1, break2]) => {
+        let whereIsBreak1 = [...graf.childNodes].indexOf(break1);
+        let whereIsBreak2 = [...graf.childNodes].indexOf(break2);
+
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+
+        // get all the nodes leading up to first break, wrap them in a p tag
+        for (let i = 0; i < whereIsBreak1; i++) {
+          let node = graf.childNodes[i];
+          // append a clone so the `childNodes` NodeList isn't mutated
+          p1.appendChild(node.cloneNode());
+        }
+        // get all the nodes after the second break, wrap them in a p tag
+        for (let i = whereIsBreak2 + 1; i < graf.childNodes.length; i ++) {
+          let node = graf.childNodes[i];
+          // append a clone so the `childNodes` NodeList isn't mutated
+          p2.appendChild(node.cloneNode());
+        }
+
+        // insert them before the graf and remove the graf
+        root.insertBefore(p2, graf);
+        root.insertBefore(p1, p2);
+        root.removeChild(graf);
+      });
+    }
+  }
+
+  /**
+    Finds all matches for given selector and updates any `src` attributes that start with `http://` to `https://`.
+
+    @param selector {String} CSS selector
+    @return {void}
+  */
+  secureSrc(selector) {
+    this.querySelectorAll(selector).forEach(node => {
+      if (node.src) {
+        node.src = node.src.replace(/^http?:/, 'https:');
+      }
+    })
+  }
+
+  /**
+    Finds all anchor links within current DOM tree and updates them to open in a new window if they point to a different domain.
+
+    @method externalizeAnchors
+    @return {void}
+  */
+  externalizeAnchors() {
+    this.querySelectorAll('a').forEach(anchor => {
+      let targetDomain = anchor.host;
+      let currentDomain = window.location.host;
+      if (targetDomain !== currentDomain) {
+        // open in a new window
+        anchor.setAttribute('target', '_blank');
+        anchor.setAttribute('rel', 'noopener');
+      }
+    });
+  }
+}
+
+/**
+  Default options for `DomFixer.rescueOrphans` method.
+
+  @const DEFAULT_ORPHAN_OPTIONS
+  @type {Object}
+*/
+const DEFAULT_ORPHAN_OPTIONS = {
+  orphanRoot: 'p'
+};
+
+/**
+  Any node found in this list to be adjacent to an orphan will be included with the orphan when it is given a new root.
+
+  @const ORPHAN_NODES
+  @type {Array}
+*/
+const ORPHAN_NODES = ['SPAN', 'A', 'BR', '#text'];
+
+/**
+  Creates a TreeWalker configured to find `#text` nodes that contain only whitespace.
+
+  @function getWhiteWalker
+  @param root {Node} root node from which to look below
+  @return tree {TreeWalker}
+*/
+const getWhiteWalker = root =>
+  document.createTreeWalker(root, NodeFilter.SHOW_TEXT, {
+    acceptNode: node =>
+      node.textContent.trim() === '' ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP,
+  });
+
+/**
+  Creates a TreeWalker configured to find `#text` nodes that are one level below the given `root` node, nodes considered to be orphaned.
+
+  @function getOrphanWalker
+  @param root {Node}
+  @return tree {TreeWalker}
+*/
+const getOrphanWalker = root =>
+  document.createTreeWalker(root, NodeFilter.SHOW_TEXT, {
+    acceptNode: node => [...root.childNodes].includes(node),
+  });
+
+/**
+  Creates a TreeWalker configured to find paragraph elements that contain adjacent line breaks (`<br/>`).
+
+  @function getBrokenWalker
+  @param root {Node}
+  @return tree {TreeWalker}
+*/
+const getBrokenWalker = root =>
+  document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, {
+    acceptNode: node => {
+      if (node.nodeName === 'P' && node.querySelector('br + br')) {
+        return NodeFilter.FILTER_ACCEPT;
+      } else {
+        return NodeFilter.FILTER_SKIP;
+      }
+    }
+  });
---
+++ tests/unit/utils/dom-fixer-test.js
@@ -0,0 +1,120 @@
+import DomFixer from 'gothamist-web-client/utils/dom-fixer';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | dom-fixer', function() {
+
+  test('it works', function(assert) {
+    assert.expect(4);
+    try {
+      new DomFixer();
+    } catch(e) {
+      assert.ok('throws if no string is passed in');
+    }
+
+    const HTML = '<p>hello <strong>world</strong></p>';
+    let domFixer = new DomFixer(HTML);
+
+    assert.ok(domFixer.nodes instanceof DocumentFragment, 'makes a document fragment out of some text');
+    assert.equal(domFixer.nodes.firstElementChild.outerHTML, HTML);
+
+    assert.deepEqual(domFixer.querySelector('p'), domFixer.nodes.querySelector('p'), 'calling querySelector on the instance is the same as calling it on the nodes');
+  });
+
+  test('removeEmptyNodes deletes empty text nodes', function(assert) {
+    const HTML = `
+      <p>hello</p>
+      <p>world</p>
+    `;
+    let domFixer = new DomFixer(HTML);
+
+    assert.equal(5, domFixer.nodes.childNodes.length, 'should be 5 nodes in the parsed output');
+    assert.deepEqual(['#text', 'P', '#text', 'P', '#text'], [...domFixer.nodes.childNodes].map(n => n.nodeName));
+    assert.deepEqual(['', 'hello', '', 'world', ''], [...domFixer.nodes.childNodes].map(n => n.textContent.trim()));
+
+    domFixer.removeEmptyNodes();
+
+    assert.equal(2, domFixer.nodes.childNodes.length, 'should only be 2 nodes after removing empty text nodes');
+    assert.deepEqual(['P', 'P'], [...domFixer.nodes.childNodes].map(n => n.nodeName));
+    assert.deepEqual(['hello', 'world'], [...domFixer.nodes.childNodes].map(n => n.textContent.trim()));
+  });
+
+  test('rescueOrphans moves text nodes into a <p/>', function(assert) {
+    const HTML = `<p>hello</p>
+      foo bar baz<br>
+      biz baz buz
+      <p>world</p>
+      <a>click here</a> for more`;
+    let domFixer = new DomFixer(HTML);
+
+    assert.equal(8, domFixer.nodes.childNodes.length, 'should be 9 nodes in the parsed output');
+    assert.deepEqual([
+      'P', '#text', 'BR', '#text', 'P', '#text', 'A', '#text'
+    ], [...domFixer.nodes.childNodes].map(n => n.nodeName));
+
+    domFixer.rescueOrphans();
+
+    assert.equal(4, domFixer.nodes.childNodes.length, 'should be 4 nodes after rescuing orphans');
+    assert.deepEqual(['P', 'P', 'P', 'P'], [...domFixer.nodes.childNodes].map(n => n.nodeName));
+  });
+
+  test('unbreakParagraphs splits a paragraph with double line breaks into two paragraphs', function(assert) {
+    assert.expect(7);
+    const HTML = `<p>
+      hello world
+      <br>
+      <br>
+      foo bar baz
+    </p>`;
+    let domFixer = new DomFixer(HTML);
+
+    assert.equal(1, domFixer.nodes.childNodes.length);
+    assert.equal('P', domFixer.nodes.firstElementChild.nodeName);
+
+    try {
+      domFixer.unbreakParagraphs();
+    } catch(e) {
+      assert.equal("Empty nodes must be removed before paragraphs can be split.", e.message);
+    }
+
+    domFixer.removeEmptyNodes();
+    domFixer.unbreakParagraphs();
+
+    assert.equal(2, domFixer.nodes.childNodes.length);
+    assert.deepEqual(['P', 'P'], [...domFixer.nodes.childNodes].map(n => n.nodeName));
+
+    assert.equal('hello world', domFixer.nodes.children[0].textContent.trim());
+    assert.equal('foo bar baz', domFixer.nodes.children[1].textContent.trim());
+  });
+
+  test('secureSrc secures the src attributes of the passed in selector', function(assert) {
+    const HTML = `
+      <img src="http://picsum.photos/200/300" />
+      <img src="http://picsum.photos/400" />
+
+      <iframe src="http://gothaimst.com" />
+      <iframe src="http://google.com" />
+    `;
+    let domFixer = new DomFixer(HTML);
+
+    assert.ok([...domFixer.querySelectorAll('img, iframe')].every(n => n.src.startsWith('http://')));
+
+    domFixer.secureSrc('img');
+    assert.ok([...domFixer.querySelectorAll('img')].every(n => n.src.startsWith('https://')));
+
+    domFixer.secureSrc('iframe');
+    assert.ok([...domFixer.querySelectorAll('iframe')].every(n => n.src.startsWith('https://')));
+  });
+
+  test('externalizeAnchors makes anchors open in a new window', function(assert) {
+    const HTML = `
+      <a href="${window.location}/foo" id="internal">click here</a>
+      <a href="https://google.com" id="external">click here too</a>
+    `;
+    let domFixer = new DomFixer(HTML);
+
+    domFixer.externalizeAnchors();
+
+    assert.dom(domFixer.querySelector('#external')).hasAttribute('target', '_blank');
+    assert.dom(domFixer.querySelector('#external')).hasAttribute('rel', 'noopener');
+  })
+});
