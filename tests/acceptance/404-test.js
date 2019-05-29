230cd7e4d915505ea08efb419c753672d5af3011
Brian Whitton <bwhitton@wnyc.org>
2019-05-29 09:35:06 -0400

    assert that navigating to a bad route renders a 404

--- tests/acceptance/404-test.js
+++ tests/acceptance/404-test.js
@@ -1,7 +1,9 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+import config from 'gothamist-web-client/config/environment';
 
 module('Acceptance | 404', function(hooks) {
   setupApplicationTest(hooks);
@@ -14,5 +16,25 @@ module('Acceptance | 404', function(hooks) {
     assert.equal(currentURL(), '/nope');
 
     assert.dom('[data-test-block]').exists({count: 4}, 'renders 4 popular stories');
+  });
+
+  test('navigating to 404 page', async function(assert) {
+    const PERMALINK = 'foo';
+    server.get(`${config.apiServer}/topics/search`, function(schema, request) {
+      let {
+        record,
+      } = request.queryParams;
+      if (record === PERMALINK) {
+        return {entries: []};
+      }
+      return schema.articles.all();
+    });
+    server.create('article', {permalink: PERMALINK, id: '1'});
+
+    await visit('/');
+    await click('[data-test-block="1"] .c-block__title-link');
+
+    assert.equal(currentURL(), `/${PERMALINK}`);
+    assert.dom('[data-test-404-heading]').hasText("The page you're looking for doesn't appear to exist.");
   });
 });
