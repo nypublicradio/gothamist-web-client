7835241167c5c5fcb6b3ce71956a69a23ab2b3c3
walsh9 <walsh9@gmail.com>
2019-05-30 12:11:44 -0400

    Add tests for inside ad tag

--- tests/integration/components/ad-tag-inside/component-test.js
+++ tests/integration/components/ad-tag-inside/component-test.js
@@ -3,20 +3,40 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
+
+const oneHundredWords = new Array(50).fill('dummy text ').join('');
+
 module('Integration | Component | ad-tag-inside', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function() {
+    this.oneHundredWords = oneHundredWords;
+  });
 
-    // Template block usage:
+  test('it renders in an article', async function(assert) {
     await render(hbs`
       {{#ad-tag-inside}}
-        template block text
+        <div class='c-article__body'>
+          template block text
+        </div>
       {{/ad-tag-inside}}
     `);
 
+    assert.dom('.c-article__body #inserted-ad .ad-tag-wide').exists()
     assert.dom('[id^=ad_]').exists()
   });
+
+  test('it renders in the specified container', async function(assert) {
+    await render(hbs`
+      {{#ad-tag-inside containerSelector='.special-div'}}
+        <div class='special-div'>
+          template block text
+        </div>
+      {{/ad-tag-inside}}
+    `);
+
+    assert.dom('.special-div #inserted-ad .ad-tag-wide').exists()
+    assert.dom('[id^=ad_]').exists()
+  });
+
 });
