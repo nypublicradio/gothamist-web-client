e2d9d1e7565f4ce4961557f36428a87ccec61e33
Brian Whitton <bwhitton@wnyc.org>
2019-05-30 10:10:43 -0400

    don't use UA strings for health checks anymore

--- fastboot.js
+++ fastboot.js
@@ -3,7 +3,6 @@ const fastboot = require('nypr-fastboot');
 const server = fastboot({
   bucket: process.env.AWS_BUCKET,
   manifestKey: process.env.FASTBOOT_MANIFEST,
-  healthCheckerUA: 'ELB-HealthChecker',
   sentryDSN: process.env.SENTRY_DSN,
   env: process.env.ENV,
   serviceName: process.env.SERVICE_NAME,
