bd9111ab0b4f6da988f021a93a8375ba4de6cd54
Brian Whitton <bwhitton@wnyc.org>
2019-06-10 15:46:29 -0400

    only invalidate /assets/

--- config/deploy.js
+++ config/deploy.js
@@ -42,7 +42,7 @@ module.exports = function(deployTarget) {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       distribution: process.env.AWS_CLOUDFRONT_DISTRIBUTION,
-      objectPaths: ['/*'], // invalidate all cached fastboot responses
+      objectPaths: ['/assets/*'], // invalidate all cached fastboot responses
     }
   };
 
