76a7622841bf4dd0ebeb299503c93a4df14e37e2
Brian Whitton <me@brianwhitton.com>
2019-06-12 13:21:28 -0400

    Revert "add video icons"
    This reverts commit b28cdfac8c5526f1ee1518934dde6e7e6d79d637.

--- app/models/article.js
+++ app/models/article.js
@@ -97,9 +97,6 @@ export default DS.Model.extend({
   hasMain: computed('tags', function() {
     return this.tags.includes('@main');
   }),
-  hasVideo: computed('tags', function() {
-    return this.tags.includes('@video');
-  }),
   moveableTypeId: reads('id'),
 
   breadcrumb: computed('section', function() {
