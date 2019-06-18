d0c0f7992188897c147c811e5229c21d2d169d97
walsh9 <walsh9@gmail.com>
2019-06-18 17:20:47 -0400

    Add utm values to shareMetadata

--- app/controllers/article/gallery.js
+++ app/controllers/article/gallery.js
@@ -11,17 +11,39 @@ export default Controller.extend({
 
   shareMetadata: computed('model', function() {
     return {
+      facebook: {
+        utm: {
+          source: 'Facebook',
+          medium: 'social',
+          campaign: 'Shared_Facebook',
+        },
+      },
       twitter: {
         text: this.model.title,
         via: 'gothamist',
+        utm: {
+          source: 'twitter',
+          medium: 'social',
+          campaign: 'shared_twitter',
+        },
       },
       reddit: {
         title: this.model.title,
+        utm: {
+          source: 'reddit',
+          medium: 'social',
+          campaign: 'shared_reddit',
+        },
       },
       email: {
         subject: `From Gothamist: ${this.model.title}`,
         body: `${this.model.title}
         {{URL}}`,
+        utm: {
+          source: 'email',
+          medium: 'social',
+          campaign: 'shared_email',
+        },
       }
     }
   }),
