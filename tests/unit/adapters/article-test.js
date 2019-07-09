8822d624671bf372570857ca721d40befa59e929
Brian Whitton <bwhitton@wnyc.org>
2019-07-09 16:28:16 -0400

    update article adapter

--- tests/unit/adapters/article-test.js
+++ tests/unit/adapters/article-test.js
@@ -4,6 +4,8 @@ import { module } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import test from 'ember-sinon-qunit/test-support/test';
 
+import config from 'gothamist-web-client/config/environment';
+
 module('Unit | Adapter | article', function(hooks) {
   setupTest(hooks);
 
@@ -13,12 +15,33 @@ module('Unit | Adapter | article', function(hooks) {
     assert.ok(adapter);
   });
 
-  test('queryRecord throws a 404 is entries is empty', async function(assert) {
+  test('queryRecord includes the expected args', async function() {
+    const SLUG = 'foo';
+
+    let store = this.owner.lookup('service:store');
+    let adapter = this.owner.lookup('adapter:article');
+
+    this.mock(adapter)
+      .expects('ajax')
+      .once()
+      .withArgs(`${config.cmsServer}/api/v2/pages/`, 'GET', {data: {
+        type: 'news.ArticlePages',
+        fields: '*',
+        limit: 1,
+        slug: SLUG,
+      }})
+      .resolves({items: [{}]});
+
+
+    await adapter.queryRecord(store, store.modelFor('article'), {slug: SLUG});
+  });
+
+  test('queryRecord throws a 404 is items is empty', async function(assert) {
     let store = this.owner.lookup('service:store');
     let adapter = this.owner.lookup('adapter:article');
 
     this.stub(adapter, 'ajax')
-      .resolves({entries: []});
+      .resolves({items: []});
 
     try {
       await adapter.queryRecord(store, store.modelFor('article'), {});
@@ -27,18 +50,18 @@ module('Unit | Adapter | article', function(hooks) {
     }
   });
 
-  test('query does not throw a 404 if entries is empty', async function(assert) {
+  test('query does not throw a 404 if items is empty', async function(assert) {
     let store = this.owner.lookup('service:store');
     let adapter = this.owner.lookup('adapter:article');
 
     this.stub(adapter, 'ajax')
-      .resolves({entries: []});
+      .resolves({items: []});
 
     await adapter.query(store, store.modelFor('article'), {});
     assert.ok('no error thrown');
   });
 
-  test('ajaxOptions forms query parameters according to MT requirements', function(assert) {
+  test('ajaxOptions forms query parameters according to Wagtail requirements', function(assert) {
     let adapter = this.owner.lookup('adapter:article');
 
     const URL = 'http://example.com';
@@ -56,6 +79,6 @@ module('Unit | Adapter | article', function(hooks) {
     };
 
     options = adapter.ajaxOptions(URL, 'GET', {data: queryParams});
-    assert.equal(options.url, `${URL}?foo=bar&biz=qux&biz=cats%20%26%20dogs`);
+    assert.equal(options.url, `${URL}?foo=bar&biz=qux,cats%20%26%20dogs`);
   })
 });
