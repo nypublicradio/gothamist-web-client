3f350f5a890750d9e59fccf5db3b5a1fc1c517b7
Brian Whitton <bwhitton@wnyc.org>
2019-07-11 18:54:21 -0400

    update article model to match wagtail's news.ArticlePage class
    this will be abstracted into an addon once the kinks are worked out

--- tests/unit/adapters/article-test.js
+++ tests/unit/adapters/article-test.js
@@ -4,7 +4,7 @@ import { module } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import test from 'ember-sinon-qunit/test-support/test';
 
-import config from 'gothamist-web-client/config/environment';
+import { DEFAULT_QUERY_PARAMS } from 'gothamist-web-client/adapters/article';
 
 module('Unit | Adapter | article', function(hooks) {
   setupTest(hooks);
@@ -15,25 +15,20 @@ module('Unit | Adapter | article', function(hooks) {
     assert.ok(adapter);
   });
 
-  test('queryRecord includes the expected args', async function() {
-    const SLUG = 'foo';
+  test('ajaxOptions adds the expected params', function(assert) {
+    const URL = 'https://example.com';
+    const QP = {slug: 'foo'};
 
-    let store = this.owner.lookup('service:store');
+    const EXPECTED = Object.entries({
+      ...DEFAULT_QUERY_PARAMS,
+      ...QP,
+    }).map(([key, val]) => `${key}=${val}`).join('&');
+
     let adapter = this.owner.lookup('adapter:article');
 
-    this.mock(adapter)
-      .expects('ajax')
-      .once()
-      .withArgs(`${config.cmsServer}/api/v2/pages/`, 'GET', {data: {
-        type: 'news.ArticlePages',
-        fields: '*',
-        limit: 1,
-        slug: SLUG,
-      }})
-      .resolves({items: [{}]});
+    const ajaxOptions = adapter.ajaxOptions(URL, 'GET', {data: QP});
 
-
-    await adapter.queryRecord(store, store.modelFor('article'), {slug: SLUG});
+    assert.equal(`${URL}?${EXPECTED}`, ajaxOptions.url);
   });
 
   test('queryRecord throws a 404 is items is empty', async function(assert) {
@@ -67,18 +62,10 @@ module('Unit | Adapter | article', function(hooks) {
     const URL = 'http://example.com';
     let queryParams = {
       foo: 'bar',
-      biz: 'qux'
+      biz: ['qux', 'cats & dogs'],
     };
 
     let options = adapter.ajaxOptions(URL, 'GET', {data: queryParams});
-    assert.equal(options.url, `${URL}?foo=bar&biz=qux`);
-
-    queryParams = {
-      foo: 'bar',
-      biz: ['qux', 'cats & dogs']
-    };
-
-    options = adapter.ajaxOptions(URL, 'GET', {data: queryParams});
-    assert.equal(options.url, `${URL}?foo=bar&biz=qux,cats%20%26%20dogs`);
+    assert.ok(options.url.match('&biz=qux,cats%20%26%20dogs'), 'array params are joined by a comma');
   })
 });
