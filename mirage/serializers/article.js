fdfdb79c3dc49c1664e92faf66e70aaee953d9fd
Brian Whitton <bwhitton@wnyc.org>
2019-07-11 18:51:59 -0400

    updates mirage serializer to match wagtail output

--- mirage/serializers/article.js
+++ mirage/serializers/article.js
@@ -2,16 +2,17 @@ import { Serializer } from 'ember-cli-mirage';
 import { underscore } from '@ember/string';
 
 export default Serializer.extend({
-  keyForCollection: () => 'entries',
+  keyForCollection: () => 'items',
   keyForAttribute: attr => underscore(attr),
 
-  serialize(object, request) {
+  serialize(object/*, request*/) {
     let json = Serializer.prototype.serialize.apply(this, arguments);
 
     return {
       ...json,
-      total_entries: object.models.length,
-      listed_entries: parseInt(request.queryParams.count, 10),
+      meta: {
+        total_count: object.models.length,
+      },
     };
   }
 });
