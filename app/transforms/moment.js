01923d416270d2167b9887f3578e8253f36719a2
Brian Whitton <bwhitton@wnyc.org>
2019-07-11 13:58:53 -0700

    adds a transform for moment fields

---
+++ app/transforms/moment.js
@@ -0,0 +1,27 @@
+import moment from 'moment';
+import DS from 'ember-data';
+
+export default DS.Transform.extend({
+  // return an in-memory value for the given serialized primitive
+  deserialize(string, ops = {}) {
+    let date = moment(string, ops.inputFormat);
+    if (ops.utc) {
+      date.utc();
+    }
+    return date;
+  },
+
+  // return a serialized value suitable for JSON over the wire
+  serialize(date, ops = {}) {
+    if (moment.isMoment(date)) {
+      if (ops.utc) {
+        date.utc();
+      } else {
+        date.local();
+      }
+      return date.format(ops.outputFormat);
+    } else {
+      return date;
+    }
+  }
+});
