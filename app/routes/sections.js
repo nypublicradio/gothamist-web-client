2caf459be117ece326e3bbc66b2375c28d611160
Brian Whitton <bwhitton@wnyc.org>
2019-07-16 14:35:28 -0400

    refactor sections route a bit to move certain things onto the controller

--- app/routes/sections.js
+++ app/routes/sections.js
@@ -3,10 +3,7 @@ import RSVP from 'rsvp';
 import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 
-import fade from 'ember-animated/transitions/fade';
-
 import addCommentCount from '../utils/add-comment-count';
-
 
 const { hash } = RSVP;
 export const COUNT = 12;
@@ -56,25 +53,13 @@ export default Route.extend({
     });
   },
 
-  setupController(controller, model) {
-    this._super(...arguments);
-
-    controller.setProperties({
-      query: {
-        descendant_of: model.section.id,
-        show_on_index_listing: true,
-        limit: COUNT,
-      },
-      transition: fade,
-    });
-
+  afterModel(model) {
     if (this.fastboot.isFastBoot) {
       return;
-    } else {
-      addCommentCount(model.river);
-
-      controller.set('addComments', results => (addCommentCount(results), results));
     }
+
+    addCommentCount(model.river);
+    addCommentCount(model.featured);
   },
 
   actions: {
