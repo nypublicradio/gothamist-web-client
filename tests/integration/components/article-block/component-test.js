6f4b1935923e1bb6e265b8ed3ace0b11f903f919
Brian Whitton <bwhitton@wnyc.org>
2019-07-17 17:02:51 -0400

    failing acceptance, integration, and unit tests for section

--- tests/integration/components/article-block/component-test.js
+++ tests/integration/components/article-block/component-test.js
@@ -15,12 +15,17 @@ module('Integration | Component | article-block', function(hooks) {
   test('it renders', async function(assert) {
     const recent = moment().subtract(1, 'day');
     const IMAGE_ID = 100;
+    const SECTION = 'News';
     const GOTHAMIST_ITEM = {
       title: 'Article Title',
       description: 'Summary of the article',
       publishedMoment: recent,
       thumbnail: {
         id: IMAGE_ID,
+      },
+      section: {
+        slug: 'news',
+        title: SECTION,
       }
     };
 
@@ -41,6 +46,8 @@ module('Integration | Component | article-block', function(hooks) {
 
     assert.dom('[data-test-timestamp]').hasText(recent.format(TIMESTAMP_FORMAT_NO_YEAR));
 
+    assert.dom('[data-test-section-label]').hasText(SECTION);
+
     await render(hbs`
       <ArticleBlock
         @article={{item}}
@@ -58,7 +65,7 @@ module('Integration | Component | article-block', function(hooks) {
     const GOTHAMIST_ITEM = {
       title: 'Article Title',
       description: 'Summary of the article',
-      section: {label: 'News', basename: 'news'},
+      section: {title: 'News', slug: 'news'},
     };
 
     this.set('item', GOTHAMIST_ITEM);
@@ -68,21 +75,21 @@ module('Integration | Component | article-block', function(hooks) {
     assert.dom('.c-block__media img').hasAttribute('srcset', FALLBACK_THUMBNAIL.news.srcSet, 'uses fallback based on section: news');
     assert.dom('.c-block__media source').hasAttribute('srcset', FALLBACK_THUMBNAIL.news.srcM, 'uses fallback based on section: news');
 
-    this.set('item.section.basename', 'food');
+    this.set('item.section.slug', 'food');
     await render(hbs`<ArticleBlock @article={{item}} />`);
 
     assert.dom('.c-block__media img').hasAttribute('src', FALLBACK_THUMBNAIL.food.srcS, 'uses fallback based on section: food');
     assert.dom('.c-block__media img').hasAttribute('srcset', FALLBACK_THUMBNAIL.food.srcSet, 'uses fallback based on section: food');
     assert.dom('.c-block__media source').hasAttribute('srcset', FALLBACK_THUMBNAIL.food.srcM, 'uses fallback based on section: food');
 
-    this.set('item.section.basename', 'arts & entertainment');
+    this.set('item.section.slug', 'arts & entertainment');
     await render(hbs`<ArticleBlock @article={{item}} />`);
 
     assert.dom('.c-block__media img').hasAttribute('src', FALLBACK_THUMBNAIL['arts & entertainment'].srcS, 'uses fallback based on section: arts');
     assert.dom('.c-block__media img').hasAttribute('srcset', FALLBACK_THUMBNAIL['arts & entertainment'].srcSet, 'uses fallback based on section: arts');
     assert.dom('.c-block__media source').hasAttribute('srcset', FALLBACK_THUMBNAIL['arts & entertainment'].srcM, 'uses fallback based on section: arts');
 
-    this.set('item.section.basename', undefined);
+    this.set('item.section.slug', undefined);
     await render(hbs`<ArticleBlock @article={{item}} />`);
 
     assert.dom('.c-block__media img').hasAttribute('src', FALLBACK_THUMBNAIL[undefined].srcS, 'no category src');
--- tests/unit/models/article-test.js
+++ tests/unit/models/article-test.js
@@ -1,6 +1,9 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
+import { ANCESTRY } from '../fixtures/article-fixtures';
+
+
 module('Unit | Model | article', function(hooks) {
   setupTest(hooks);
 
@@ -9,5 +12,15 @@ module('Unit | Model | article', function(hooks) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('article', {});
     assert.ok(model);
+  });
+
+  // test computeds
+  test('section is computed from ancestry', function(assert) {
+    let store = this.owner.lookup('service:store');
+    let model = store.createRecord('article', {
+      ancestry: ANCESTRY,
+    });
+
+    assert.deepEqual(model.section, {slug: 'news', title: 'News'});
   });
 });
