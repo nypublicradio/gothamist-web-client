fd43cdbf8f89dc5eead2d20b81cb4dd5aac448b2
Brian Whitton <me@brianwhitton.com>
2019-07-22 09:57:41 -0400

    adds a `query` method to the `Page` adapter to use for searches

--- app/adapters/page.js
+++ app/adapters/page.js
@@ -1,6 +1,12 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
+  query(_store, _type, query = {}) {
+    let url = this.buildURL('page');
+    url = `${url.replace('pages', 'search')}?q=${query.q}`;
+
+    return this.ajax(url);
+  },
   queryRecord(_store, _type, query = {}) {
     let url = this.buildURL('page');
     url = `${url}find/?html_path=${query.html_path}`;
