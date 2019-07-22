ad90ad0f5b23b81a43fa3bcb8a124c181d3a40db
Brian Whitton <me@brianwhitton.com>
2019-07-22 10:30:48 -0400

    support polymorphism in Page serializer
    if a `query` request returns article objects, they will be properly serialized

--- app/serializers/page.js
+++ app/serializers/page.js
@@ -1,8 +1,16 @@
 import ApplicationSerializer from './application';
 
+import { WAGTAIL_MODEL_TYPE as ARTICLE } from '../models/article';
+
+
+const TYPES = {
+  [ARTICLE]: 'article',
+  // [PERSON]: 'person',
+};
 
 export default ApplicationSerializer.extend({
-  modelNameFromPayloadKey: () => 'page',
+  // enable polymorphic responses
+  modelNameFromPayloadKey: key => key === 'items' ? 'page' : key,
 
   normalizeQueryRecordResponse(store, PageModel, payload) {
     payload = {
@@ -11,4 +19,14 @@ export default ApplicationSerializer.extend({
 
     return this._super(store, PageModel, payload);
   },
+
+  normalizeQueryResponse(store, PageModel, payload, id, requestType) {
+    payload.items = payload.items.map(({ result }) => {
+      return {
+        type: TYPES[result.meta.type], // enable polymorphic responses
+        ...result,
+      };
+    })
+    return this._super(store, PageModel, payload, id, requestType);
+  }
 });
