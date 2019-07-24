18c0174ce75c15b6088895892482744ceae3b0ae
walsh9 <walsh9@gmail.com>
2019-07-24 11:59:25 -0400

    Refactor product banner

--- app/serializers/system-messages.js
+++ app/serializers/system-messages.js
@@ -1,28 +1,37 @@
+import ApplicationSerializer from './application';
 import DS from 'ember-data';
-import { camelize, underscore } from '@ember/string';
+import { camelize } from '@ember/string';
 
-export default DS.RESTSerializer.extend({
+const normalizeProductBanner = function(banner) {
+  return {
+    id: banner.id,
+    type: 'product-banner',
+    // move 'values' to 'attributes' and camelize keys
+    attributes: {...Object.fromEntries(
+      Object.entries(banner.value).map(([k, v]) => [camelize(k), v])
+    )}
+  }
+};
+
+export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
+  attrs: {
+    productBanners: { embedded: 'always' }
+  },
   modelNameFromPayloadKey: () => 'system-messages',
-  keyForAttribute(attr) {
-    return underscore(attr);
-  },
-  normalizeFindRecordResponse(store, galleryClass, payload, id) {
-    let banners = payload.product_banners.map(original => {
-      let banner = {
-        guid: original.id
-      }
-      Object.keys(original.value).forEach(key => {
-        banner[camelize(key)] = original.value[key];
-      });
-      return banner;
-    });
-    let data = {
-      id,
-      type: 'system_messages',
-      attributes: {
-        productBanners: banners
-      }
-    }
-    return {data};
-  },
+  normalizeFindRecordResponse(store, primaryModelClass, payload, id) {
+  return {
+      data: {
+        id,
+        type: 'system-messages',
+        relationships: {
+          productBanners: {
+            data: payload.product_banners.map(banner =>
+              ({ id: banner.id, type: 'product-banner' })
+            )
+          }
+        }
+      },
+      included: payload.product_banners.map(normalizeProductBanner)
+    };
+  }
 });
