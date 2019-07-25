a936c60fb39bc26d0172ab36ec11ce305dc61e8e
walsh9 <walsh9@gmail.com>
2019-07-25 09:57:51 -0400

    Update mirage for system messages

---
+++ mirage/factories/product-banner.js
@@ -0,0 +1,31 @@
+import { Factory, faker } from 'ember-cli-mirage';
+import { capitalize } from '@ember/string'
+
+export default Factory.extend({
+  id() {
+    return faker.random.uuid();
+  },
+  title() {
+    return capitalize(faker.lorem.words());
+  },
+  description() {
+    return `<p>${faker.lorem.sentence(5)}</p>`;
+  },
+  button_text() {
+    return capitalize(faker.lorem.words());
+  },
+  button_link() {
+    return faker.internet.url();
+  },
+  "frequency": 8,
+  "location": "TOP",
+  afterCreate(productBanner, server) {
+    // all product banners get added the the first system message
+    // (we should only have one per site, so we only need one here)
+    let systemMessage = server.schema.systemMessages.first();
+    if (systemMessage) {
+      productBanner.update({systemMessage});
+      systemMessage.productBanners.add(productBanner);
+    }
+  }
+});
--- mirage/factories/system-message.js
+++ mirage/factories/system-message.js
@@ -1,32 +1,4 @@
-import { Factory, faker } from 'ember-cli-mirage';
-import { capitalize } from '@ember/string'
-
-const createProductBanner = function() {
-  return {
-    "id": faker.random.uuid(),
-    "type": "product_banner",
-    "value": {
-      "title": capitalize(faker.lorem.words()),
-      "description": `<p>${faker.lorem.sentence(5)}</p>`,
-      "button_text": capitalize(faker.lorem.words()),
-      "button_link": faker.internet.url(),
-      "frequency": 8,
-      "location": "TOP"
-    }
-  }
-}
+import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  id(i) {
-    return i + 1;
-  },
-  meta(i) {
-    return {
-      type : "utils.SystemMessagesSettings",
-      'detail_url': `http://localhost/api/v2/system_messages/${i + 1}/`
-    };
-  },
-  productBanners() {
-    return [createProductBanner()];
-  }
 });
