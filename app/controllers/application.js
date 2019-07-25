f8808e2e0b3257490f1c38147b36f2139f876f44
Brian Whitton <bwhitton@wnyc.org>
2019-07-25 13:19:26 -0400

    much simpler search action now
    just transition to the search template and let the model hook do the 
    work

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -42,22 +42,7 @@ export default Controller.extend({
    @param {String} q The search query
    @return {void}
   */
-  search(value) {
-    // if there are 3 or more articles for the given query
-    // go to the tag listing for that term
-    // otherwise do a full site search
-    this.store.query('article', {
-      index: 'gothamist',
-      offset: 2, // start at 3
-      count: 1, // only  need to know if there's at least 1 entry (i.e. 3 or more is valid)
-      term: value,
-    })
-    .then(result => {
-      if (result.meta.count > 0) {
-        this.router.transitionTo('tags', value);
-      } else {
-        this.router.transitionTo('search', {queryParams: {q: value}});
-      }
-    });
+  search(q) {
+    this.router.transitionTo('search', {queryParams: { q }});
   }
 });
