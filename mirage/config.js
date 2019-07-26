dcc3f4ba776fd73b8baa8adffc52d76ba7a5dbfa
Brian Whitton <me@brianwhitton.com>
2019-07-26 12:16:20 -0400

    **TEMPORARY** need to splice in `articles` to article page lookup
    once the DB structure is updated, this can go away

--- mirage/config.js
+++ mirage/config.js
@@ -77,6 +77,9 @@ export default function() {
   this.get('/api/v2/pages/find', (schema, request) => {
     let { html_path } = request.queryParams;
 
+    // HACK for supporting demo page structure
+    // currently has `articles` in the middle
+    html_path = html_path.replace('/articles', '');
     let found = searchAllCollections({ html_path }, schema);
 
     return found || new Response(404);
