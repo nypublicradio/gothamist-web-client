8861d8caa89c41362ac9605ac99acdfa622d3ebf
Brian Whitton <me@brianwhitton.com>
2019-07-26 12:13:02 -0400

    use slug to construct the `html_url`
    derive `html_path` the same way as an article

--- mirage/factories/index-page.js
+++ mirage/factories/index-page.js
@@ -10,25 +10,24 @@ import {
 
 
 export default Factory.extend({
-  meta: () => ({
-    first_published_at: moment.utc(faker.date.recent()).format(CMS_TIMESTAMP_FORMAT),
-    type: "standardpages.IndexPage",
-    detail_url: "http://cms.demo.nypr.digital/api/v2/pages/5/",
-    html_url: "http://cms.demo.nypr.digital/news/",
-    slug: slug(),
-    show_in_menus: true,
-    seo_title: "",
-    search_description: "",
-    parent: {
-      id: 3,
-      meta: {
-        type: "home.HomePage",
-        detail_url: "http://cms.demo.nypr.digital/api/v2/pages/3/",
-        html_url: "http://cms.demo.nypr.digital/"
-      },
-      title: "Home"
-    }
-  }),
+  // mirage-only attrs
+  html_path() {
+    return this.meta && this.meta.html_url;
+  },
+
+  meta() {
+    const SLUG = this.slug || slug();
+    return {
+      first_published_at: moment.utc(faker.date.recent()).format(CMS_TIMESTAMP_FORMAT),
+      type: "standardpages.IndexPage",
+      detail_url: '',
+      html_url: SLUG,
+      slug: SLUG,
+      show_in_menus: true,
+      seo_title: '',
+      search_description: '',
+    };
+  },
   title: () => faker.random.words(6),
   listing_title: "",
   listing_summary: "",
@@ -44,7 +43,7 @@ export default Factory.extend({
       page.update({
         descendants: server.createList('article', COUNT * 2, {
           indexPage: page,
-          _section: page.meta.slug
+          section: page.meta.slug
         }),
       });
     }
