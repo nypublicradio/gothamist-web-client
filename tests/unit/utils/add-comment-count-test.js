b6a64a3dd3afa203f7295c12cf6c539db0bf1f8b
Brian Whitton <me@brianwhitton.com>
2019-07-29 11:34:24 -0400

    these tests can run

--- tests/unit/utils/add-comment-count-test.js
+++ tests/unit/utils/add-comment-count-test.js
@@ -1,8 +1,8 @@
 import * as fetch from 'fetch';
 
-import { module, skip } from 'qunit';
+import { module } from 'qunit';
 import { setupTest } from 'ember-qunit';
-// import test from 'ember-sinon-qunit/test-support/test';
+import test from 'ember-sinon-qunit/test-support/test';
 
 import addCommentCount, {
   QUERY_PARAMS,
@@ -12,7 +12,7 @@ import addCommentCount, {
 module('Unit | Utility | add-comment-count', function(hooks) {
   setupTest(hooks);
 
-  skip('it updates an article model', async function(assert) {
+  test('it updates an article model', async function(assert) {
     const EXPECTED = 100;
     let store = this.owner.lookup('service:store');
     const ARTICLE = store.createRecord('article', {id: 'foo'});
@@ -29,7 +29,7 @@ module('Unit | Utility | add-comment-count', function(hooks) {
     assert.equal(ARTICLE.commentCount, EXPECTED);
   });
 
-  skip('it updates a record array', async function(assert) {
+  test('it updates a record array', async function(assert) {
     const EXPECTED = [100, 200, 300];
     let store = this.owner.lookup('service:store');
     let qp = Object.keys(QUERY_PARAMS).map(k => `${k}=${QUERY_PARAMS[k]}`);
