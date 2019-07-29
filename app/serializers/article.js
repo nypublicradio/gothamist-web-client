fec8512a59742b22e969ea36c3e7b2b1f2a39145
Brian Whitton <me@brianwhitton.com>
2019-07-29 10:23:02 -0400

    assert that block types are dasherized

--- app/serializers/article.js
+++ app/serializers/article.js
@@ -12,6 +12,8 @@ export default ApplicationSerializer.extend({
     // pull out the first index for easier reference in the app
     payload.lead_asset = payload.lead_asset ? payload.lead_asset[0] : null;
 
+    // `type` key is used to look up a corresponding component
+    // ember wants component names to be dasherized
     payload.body.forEach(block => block.type = dasherize(block.type));
 
     return this._super(ArticleClass, payload);
--- tests/unit/serializers/article-test.js
+++ tests/unit/serializers/article-test.js
@@ -27,10 +27,35 @@ module('Unit | Serializer | article', function(hooks) {
     let serializer = store.serializerFor('article');
 
     const EXPECTED = 'lead asset value';
+    const FAKE_PAYLOAD = {
+      body: [], // serializer expects this
+      lead_asset: [EXPECTED],
+    }
 
-    let normalized = serializer.normalize(ArticleClass, {lead_asset: [EXPECTED]});
+    let normalized = serializer.normalize(ArticleClass, FAKE_PAYLOAD);
 
     assert.deepEqual(normalized.data.attributes.leadAsset, EXPECTED);
+  });
+
+  test('it dasherizes the `type` key of body blocks', function(assert) {
+    let store = this.owner.lookup('service:store');
+    let ArticleClass = store.modelFor('article');
+    let serializer = store.serializerFor('article');
+
+    const EXPECTED = 'social-embed';
+    const FAKE_PAYLOAD = {
+      body: [{
+        type: 'social_embed'
+      }],
+    };
+
+    let normalized = serializer.normalize(ArticleClass, FAKE_PAYLOAD);
+
+    assert.equal(
+      normalized.data.attributes.body[0].type,
+      EXPECTED,
+    );
+
   })
 
 });
