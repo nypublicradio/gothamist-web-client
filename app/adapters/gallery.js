c7211af17f6740f9f7a38befca567cac5f35aa20
Brian Whitton <me@brianwhitton.com>
2019-07-29 19:17:15 -0400

    extend gallery adapter from page adapter

--- app/adapters/gallery.js
+++ app/adapters/gallery.js
@@ -1,13 +1,4 @@
-import DS from 'ember-data';
-import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
-import config from '../config/environment';
+import PageAdapter from './page';
 
-export default DS.RESTAdapter.extend(AdapterFetch, {
-  host: config.apiServer,
-  namespace: 'platypus/api/gallery',
-  pathForType: () => '',
 
-  buildURL(modelName, id) {
-    return `${this.host}/${this.namespace}/${id}.json`;
-  }
-});
+export default PageAdapter;
