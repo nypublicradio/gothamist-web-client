994386dec7a2e3e4b581101c9bb59a768ebaf063
Brian Whitton <me@brianwhitton.com>
2019-07-29 10:22:40 -0400

    safely handle unrecognized block types

--- app/components/article-body/component.js
+++ app/components/article-body/component.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
+import { filter } from '@ember/object/computed';
+import { getOwner } from '@ember/application';
 
 export default Component.extend({
   tagName: '',
+
+  components: filter('blocks', function(block) {
+    const owner = getOwner(this);
+    return Boolean(owner.lookup(`component:article-body/${block.type}`));
+  }),
 });
