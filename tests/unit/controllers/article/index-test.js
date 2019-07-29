fab600b89b78a4f6f99c386b177cb9dff5e5f50c
Brian Whitton <me@brianwhitton.com>
2019-07-29 19:00:37 -0400

    fix this unit test lookup

--- tests/unit/controllers/article/index-test.js
+++ tests/unit/controllers/article/index-test.js
@@ -6,7 +6,7 @@ module('Unit | Controller | article', function(hooks) {
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:article/index');
+    let controller = this.owner.lookup('controller:article');
     assert.ok(controller);
   });
 });
