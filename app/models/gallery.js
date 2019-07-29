7f3b6fc1da99ec737d82a9d03736b93e8c15cef3
Brian Whitton <me@brianwhitton.com>
2019-07-29 19:25:11 -0400

    update gallery model with wagtail attrs

--- app/models/gallery.js
+++ app/models/gallery.js
@@ -1,6 +1,13 @@
 import DS from 'ember-data';
-const { Model, attr } = DS;
 
-export default Model.extend({
-  slides: attr(),
+import Page from './page';
+
+export default Page.extend({
+  slides: DS.attr(),
+
+  relatedAuthors:                   DS.attr({defaultValue: () => []}),
+  relatedContributingOrganizations: DS.attr({defaultValue: () => []}),
+
+  // relationships
+  relatedArticles: DS.hasMany('article'),
 });
