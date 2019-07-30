8656291c19609750a4deafafd01a14f0904bb7bc
Brian Whitton <bwhitton@wnyc.org>
2019-07-30 16:05:15 -0400

    specify the embedded relationship in mirage

--- mirage/serializers/gallery.js
+++ mirage/serializers/gallery.js
@@ -1,6 +1,8 @@
 import ApplicationSerializer, { cleanMirageAttrs } from './application';
 
 export default ApplicationSerializer.extend({
+  include: ['relatedArticles'], // eslint-disable-line
+
   serialize() {
     let json = ApplicationSerializer.prototype.serialize.apply(this, arguments);
 
