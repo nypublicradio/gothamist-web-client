25575ac9cfb9f736ce8c73b6227b5db9e3a16388
walsh9 <walsh9@gmail.com>
2019-07-30 17:44:41 -0400

    Add test for product banner serializer

--- mirage/factories/product-banner.js
+++ mirage/factories/product-banner.js
@@ -2,24 +2,14 @@ import { Factory, faker } from 'ember-cli-mirage';
 import { capitalize } from '@ember/string'
 
 export default Factory.extend({
-  id() {
-    return faker.random.uuid();
-  },
-  "type": "product-banner",
-  title() {
-    return capitalize(faker.lorem.words());
-  },
-  description() {
-    return `<p>${faker.lorem.sentence(5)}</p>`;
-  },
-  button_text() {
-    return capitalize(faker.lorem.words());
-  },
-  button_link() {
-    return faker.internet.url();
-  },
-  "frequency": 8,
-  "location": "TOP",
+  id: () => faker.random.uuid(),
+  type: "product-banner",
+  title: () => capitalize(faker.lorem.words()),
+  description: () => `<p>${faker.lorem.sentence(5)}</p>`,
+  button_text: () => capitalize(faker.lorem.words()),
+  button_link: () => faker.internet.url(),
+  frequency: 8,
+  location: "TOP",
   afterCreate(productBanner, server) {
     // all product banners get added the the first system message
     // (we should only have one per site, so we only need one here)
--- tests/unit/serializers/system-messages-test.js
+++ tests/unit/serializers/system-messages-test.js
@@ -4,7 +4,6 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Serializer | system messages', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let store = this.owner.lookup('service:store');
     let serializer = store.serializerFor('system-messages');
@@ -12,12 +11,91 @@ module('Unit | Serializer | system messages', function(hooks) {
     assert.ok(serializer);
   });
 
-  test('it serializes records', function(assert) {
+  test('it normalizes the response', function(assert) {
     let store = this.owner.lookup('service:store');
-    let record = store.createRecord('system-messages', {});
+    let serializer = store.serializerFor('systemMessages');
 
-    let serializedRecord = record.serialize();
+    let payload = {
+      id: 1,
+      meta: {
+        type: "utils.SystemMessagesSettings",
+        detail_url: "http://localhost/api/v2/system_messages/1/"
+      },
+      value: {
+        product_banners: []
+      }
+    };
 
-    assert.ok(serializedRecord);
+    let expected = {
+      data: {
+        id: 1,
+        type: 'system-messages',
+        relationships: {
+          productBanners: {
+            data: []
+          }
+        }
+      },
+      included: []
+    };
+
+    let normalizedJSON = serializer.normalizeFindRecordResponse(store, 'systemMessages', payload, payload.id);
+    assert.deepEqual(normalizedJSON, expected);
   });
+
+  test('it normalizes responses with product-banners', function(assert) {
+    let store = this.owner.lookup('service:store');
+    let serializer = store.serializerFor('systemMessages');
+
+    let payload = {
+      id: 1,
+      meta: {
+        type: "utils.SystemMessagesSettings",
+        detail_url: "http://localhost/api/v2/system_messages/1/"
+      },
+      product_banners: [{
+        type: "product_banner",
+        value: {
+          title:"Test Title",
+          description: "<p>Test Description</p>",
+          button_text: "Test Button",
+          button_link: "http://example.com",
+          frequency: 8,
+          location: "TOP",
+        },
+        id: "1234-abcd",
+      }]
+    };
+
+    let expected = {
+      data: {
+        id: 1,
+        type: 'system-messages',
+        relationships: {
+          productBanners: {
+            data: [
+              {id: "1234-abcd", type: "product-banner"}
+            ]
+          }
+        }
+      },
+      included: [{
+        id: "1234-abcd",
+        type: "product-banner",
+        attributes: {
+          title:"Test Title",
+          description: "<p>Test Description</p>",
+          buttonText: "Test Button",
+          buttonLink: "http://example.com",
+          frequency: 8,
+          location: "TOP",
+        }
+      }]
+    };
+
+    let normalizedJSON = serializer.normalizeFindRecordResponse(store, 'systemMessages', payload, payload.id);
+    assert.deepEqual(normalizedJSON, expected);
+
+  });
+
 });
