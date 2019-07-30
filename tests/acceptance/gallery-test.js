b11d4b70ea62b44fb0f6f26f741eb4b4e9c94fd6
Brian Whitton <bwhitton@wnyc.org>
2019-07-30 16:23:28 -0400

    gallery acceptance tests

--- tests/acceptance/gallery-test.js
+++ tests/acceptance/gallery-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, find, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -14,12 +14,57 @@ module('Acceptance | gallery', function(hooks) {
 
   hooks.afterEach(() => {
     window.block_disqus = false;
-  })
+  });
+
+  test('can navigate from an article to a gallery', async function(assert) {
+    const article = server.create('article', 'withGallery');
+    let { slides } = article.gallery;
+
+    await visit(article.html_path);
+
+    assert.dom('[data-test-gallery-lead]').exists();
+    // mirage defualt is 5 slides per gallery
+    assert.dom('[data-test-gallery-lead-view-all]').hasText('View all 5')
+
+    let preview = find('[data-test-gallery-lead-preview] img');
+    assert.ok(preview.src.match(`images/${slides[0].value.slide_image.id}/width-800`))
+
+  });
+
+  test('gallery should have the expected images', async function(assert) {
+    const gallery = server.create('gallery');
+    await visit(gallery.html_path);
+
+    gallery.slides.forEach((slide, i) => {
+      let { image:id } = slide.value.slide_image;
+
+      let img = find(`[data-test-gallery-slide="${i}"] img`);
+      assert.dom(img).hasAttribute('src');
+
+      let src = img.getAttribute('src');
+      assert.ok(
+        src.match(`images/${id}/width-420`),
+        `image src should have expected path: ${src}`
+      );
+
+      let mediumSource = find(`[data-test-gallery-slide="${i}"] [data-test-source-m]`);
+      assert.ok(
+        mediumSource.srcset.match(`images/${id}/width-800`),
+        `source srcset should have larger image path: ${mediumSource.srcset}`
+      );
+
+      let largeSource = find(`[data-test-gallery-slide="${i}"] [data-test-source-l]`);
+      assert.ok(
+        largeSource.srcset.match(`images/${id}/width-1200`),
+        `source srcset should have larger image path: ${largeSource.srcset}`
+      );
+    });
+  });
 
   test('gallery should contain 2 slides and 0 ads', async function(assert) {
-    const gallery = server.create('gallery', {section: 'food', count: 2});
+    const gallery = server.create('gallery', {count: 2});
 
-    await visit(`/${gallery.html_path}`);
+    await visit(gallery.html_path);
 
     // do a match to guard against non-determinism with ?image query string
     assert.ok(currentURL().match(`${gallery.html_path}`), 'path should include gallery');
@@ -28,7 +73,7 @@ module('Acceptance | gallery', function(hooks) {
   });
 
   test('gallery should contain 3 slides and 1 ad', async function(assert) {
-    const gallery = server.create('gallery', {section: 'food', count: 3});
+    const gallery = server.create('gallery', {count: 3});
 
     await visit(gallery.html_path);
 
@@ -38,7 +83,7 @@ module('Acceptance | gallery', function(hooks) {
   });
 
   test('gallery should contain 5 slides and 1 ads', async function(assert) {
-    const gallery = server.create('gallery', {section: 'food', count: 5});
+    const gallery = server.create('gallery', {count: 5});
 
     await visit(gallery.html_path);
 
@@ -49,7 +94,7 @@ module('Acceptance | gallery', function(hooks) {
 
 
   test('gallery should contain 6 slides and 2 ads', async function(assert) {
-    const gallery = server.create('gallery', {section: 'food', count: 6});
+    const gallery = server.create('gallery', {count: 6});
 
     await visit(gallery.html_path);
 
@@ -59,7 +104,7 @@ module('Acceptance | gallery', function(hooks) {
   });
 
   test('gallery should contain 14 slides and still only 2 ads', async function(assert) {
-    const gallery = server.create('gallery', {section: 'food', count: 14});
+    const gallery = server.create('gallery', {count: 14});
 
     await visit(gallery.html_path);
 
