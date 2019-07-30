ecc28a04ae2e9a661b60fd49dc8fcb907f47e183
Brian Whitton <me@brianwhitton.com>
2019-07-29 22:33:13 -0400

    abstract up some the common bits of mirage serialization
    create and use a cleaning util for removing mirage-only attrs

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,6 +1,43 @@
 import { Serializer } from 'ember-cli-mirage';
 import { underscore } from '@ember/string';
 
+
+const MIRAGE_ONLY = ['section', 'html_path'];
+
+/**
+  Delete passed in attributes from the given object
+  Useful for cleaning out any mirage-only attrs from the response
+
+  @function cleanMirageAttrs
+  @param obj {Object}
+  @param attrs {Array[String]} Key names to delete
+  @return {Object} cleaned object
+*/
+export function cleanMirageAttrs(obj, attrs = []) {
+  attrs.forEach(attr => delete obj[attr]);
+  return obj;
+}
+
 export default Serializer.extend({
   keyForAttribute: attr => underscore(attr),
+
+  keyForCollection: () => 'items',
+
+  serialize(object, { queryParams }) {
+    let json = Serializer.prototype.serialize.apply(this, arguments);
+
+    json.items.forEach(item => cleanMirageAttrs(item, MIRAGE_ONLY));
+
+    if (queryParams.html_path) {
+      // client is expecting a single object
+      return json.items[0];
+    } else {
+      return {
+        ...json,
+        meta: {
+          total_count: object.models.length,
+        },
+      };
+    }
+  }
 });
