1e23bbcd042731bc8415df4c524ab3dc70c2d4df
Brian Whitton <bwhitton@wnyc.org>
2019-07-30 15:54:08 -0400

    rest serializer expects payloads to be namespaced
    this part of the normlize methods can be abstracted up a level

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -2,5 +2,23 @@ import DS from 'ember-data';
 import { underscore } from '@ember/string';
 
 export default DS.RESTSerializer.extend({
+  keyForRelationship: key => underscore(key),
   keyForAttribute: key => underscore(key),
+
+  normalizeFindRecordResponse(store, Model, payload, ...rest) {
+    payload = {
+      [this.modelNameFromPayloadKey()]: payload,
+    };
+
+    return this._super(store, Model, payload, ...rest);
+  },
+
+  normalizeQueryRecordResponse(store, Model, payload) {
+    payload = {
+      [this.modelNameFromPayloadKey()]: payload,
+    };
+
+    return this._super(store, Model, payload);
+  },
+
 });
