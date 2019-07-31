2dc8b1e46aa7e4b88d8a3d8b1f10646c50d77330
Brian Whitton <me@brianwhitton.com>
2019-07-31 00:23:24 -0400

    adds a makeSizes method to gallery model
    to generate slides on the fly for different contexts

---
+++ tests/unit/fixtures/gallery-fixtures.js
@@ -0,0 +1,19 @@
+export const SLIDES = [{
+  title: 'foo title',
+  image: {
+    id: 1,
+    caption: 'foo caption',
+  },
+}, {
+  title: 'bar title',
+  image: {
+    id: 2,
+    caption: 'bar caption',
+  },
+}, {
+  title: 'baz title',
+  image: {
+    id: 3,
+    caption: 'baz caption',
+  },
+}];
