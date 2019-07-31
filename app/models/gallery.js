be7e0b598fc6b932713aa998e502a16bdbc13db3
Brian Whitton <me@brianwhitton.com>
2019-07-31 00:23:24 -0400

    adds a makeSizes method to gallery model
    to generate slides on the fly for different contexts

--- app/models/gallery.js
+++ app/models/gallery.js
@@ -1,8 +1,19 @@
 import DS from 'ember-data';
 
+// this will be a problem when the models are moved out
+import { wagtailImageUrl } from 'ember-wagtail-images';
+
 import { computed } from '@ember/object';
 
 import Page from './page';
+
+
+const HEIGHT = 'height';
+const WIDTH = 'width';
+const FILL = 'fill';
+const COVER = 'cover';
+const CONTAIN = 'contain';
+const FITS = [FILL, COVER, CONTAIN];
 
 export default Page.extend({
   // slides are normalized from the server to look like so:
@@ -10,7 +21,7 @@ export default Page.extend({
   //   title String,
   //   image Object{id Number, caption String},
   // }
-  slides: DS.attr(),
+  slides: DS.attr({deafultValue: () => []}),
 
   relatedAuthors:                   DS.attr({defaultValue: () => []}),
   relatedContributingOrganizations: DS.attr({defaultValue: () => []}),
@@ -25,4 +36,55 @@ export default Page.extend({
       route: ['author-detail', author.slug],
     }));
   }),
+
+  makeSizes(ops = {}, others = {}) {
+    return this.slides.map(slide => {
+      let sizes = Object.keys(ops)
+        .reduce((o, key) => ({
+          ...o,
+          [key]: wagtailImageUrl([slide.image, ...deriveWagtailArgs(ops[key])]),
+        }), {})
+      // sizes = Object.fromEntries(sizes);
+      return {
+        title: slide.title,
+        caption: slide.image.caption,
+        ...sizes,
+        ...others,
+      };
+    })
+  }
 });
+
+function deriveWagtailArgs(val) {
+  let args;
+  if (Array.isArray(val)) {
+    if (val.length === 2) {
+      let [ dimension, fit ] = val;
+      if (FITS.includes(fit)) {
+        args = [
+          dimension,
+          dimension,
+          fit,
+        ]; // crop to square with given fit
+      } else if (fit === HEIGHT) {
+        args = [
+          null,
+          dimension,
+          fit,
+        ]; // resize height to dimension
+      } else if (fit === WIDTH) {
+        args = [
+          dimension,
+        ];
+      }
+    }
+
+    if (!args) {
+      args = val;
+    }
+  } else {
+    args = [val];
+  }
+
+  return args;
+}
--- tests/unit/models/gallery-test.js
+++ tests/unit/models/gallery-test.js
@@ -1,6 +1,10 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
+import { wagtailImageUrl } from 'ember-wagtail-images';
+
+import { SLIDES } from '../fixtures/gallery-fixtures';
+
 module('Unit | Model | gallery', function(hooks) {
   setupTest(hooks);
 
@@ -9,5 +13,49 @@ module('Unit | Model | gallery', function(hooks) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('gallery', {});
     assert.ok(model);
+  });
+
+  test('it can produce slide sizes on demand', function(assert) {
+    let store = this.owner.lookup('service:store');
+    let model = store.createRecord('gallery', {
+      slides: SLIDES,
+    });
+
+    const slides = model.makeSizes({
+      // nothing special about these sizes
+      // each slide is given a fully-qualified URL for each key
+      // based on the param values passed in
+
+      // simple case: 1 number == resize width
+      // same as [150, 'width']
+      thumb: 150,
+
+      // two-item array: [dimension, fit]
+      srcS:  [400, 'height'],
+
+      // two-item array: [dimension, fit !== height or width]
+      // crop to a square
+      srcM:  [800, 'fill'],
+
+      // three-item array: [width, height, fit !== height or width]
+      // crop to width and height using provided fit
+      srcL:  [1200, 1000, 'cover'],
+    }, {
+      // the second object is an arbitrary list of key/values
+      // that can be added to a slide
+      width: 1200,
+    });
+
+    slides.forEach((slide, i) => {
+      assert.deepEqual(slide, {
+        title: model.slides[i].title,
+        caption: model.slides[i].image.caption, // always brings in the caption
+        width: 1200, // include the abitrary values
+        thumb: wagtailImageUrl([model.slides[i].image, 150]), // width-150
+        srcS: wagtailImageUrl([model.slides[i].image, null, 400]), // height-400
+        srcM: wagtailImageUrl([model.slides[i].image, 800, 800]), // fill-800x800
+        srcL: wagtailImageUrl([model.slides[i].image, 1200, 1000, 'cover']), // min-1200x1000
+      })
+    })
   });
 });
