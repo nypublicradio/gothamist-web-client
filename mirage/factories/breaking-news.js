dec2edcf1f0b433027e47462c4b9fb71bf28df4b
walsh9 <walsh9@gmail.com>
2019-08-01 14:44:08 -0400

    Timestamps

--- mirage/factories/breaking-news.js
+++ mirage/factories/breaking-news.js
@@ -1,20 +1,15 @@
 import { Factory, faker } from 'ember-cli-mirage';
 import { capitalize } from '@ember/string'
+import moment from 'moment';
+import { CMS_TIMESTAMP_FORMAT } from './consts';
 
 export default Factory.extend({
-  id() {
-    return faker.random.uuid();
-  },
+  id:() => faker.random.uuid(),
   type: 'breaking-news',
-  title() {
-    return capitalize(faker.lorem.words());
-  },
-  link() {
-    return faker.internet.url();
-  },
-  description() {
-    return `<p>${faker.lorem.sentence(5)}</p>`;
-  },
+  title: () => capitalize(faker.lorem.words()),
+  link: () => faker.internet.url(),
+  description: () => `<p>${faker.lorem.sentence(5)}</p>`,
+  time: () => moment.utc(faker.date.recent()).format(CMS_TIMESTAMP_FORMAT),
   afterCreate(breakingNews, server) {
     // breaking news gets added to sitewide-components
     let sitewideComponent = server.schema.sitewideComponents.first();
