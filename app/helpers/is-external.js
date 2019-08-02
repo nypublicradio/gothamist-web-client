7ba4bd998997de4d8001f2de667af1bfa2ab2da4
walsh9 <walsh9@gmail.com>
2019-08-02 12:13:39 -0400

    implement isexternal helper

--- app/helpers/is-external.js
+++ app/helpers/is-external.js
@@ -1,7 +1,19 @@
 import { helper } from '@ember/component/helper';
 
-export function isExternal(params/*, hash*/) {
-  return params;
+export function isExternal([ url ]/*, hash*/) {
+  if (window && window.location) {
+    let base = window.location.origin;
+    if (url.startsWith(base)) {
+      return false;
+    }
+    if (url.startsWith('/')) {
+      return false;
+    }
+    if (url === '') {
+      return false;
+    }
+  }
+  return true;
 }
 
 export default helper(isExternal);
