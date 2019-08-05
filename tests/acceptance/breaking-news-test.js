97513b5598ba981b3942ec6e30fd97dfb79eef7b
walsh9 <walsh9@gmail.com>
2019-08-05 09:44:20 -0400

    Acceptance tests

--- tests/acceptance/breaking-news-test.js
+++ tests/acceptance/breaking-news-test.js
@@ -1,13 +1,50 @@
-import { module, skip } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | breaking news', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
-  skip('visiting /breaking-news', async function(assert) {
-    await visit('/breaking-news');
+  hooks.beforeEach(() => {
+    server.create('sitewide-components');
+  });
 
-    assert.equal(currentURL(), '/breaking-news');
+  test('breaking news appears on the home page', async function(assert) {
+    server.create('breaking-news');
+
+    await visit('/');
+
+    assert.dom('[data-test-breaking-news]').exists({count: 1});
+  });
+
+  test('multiple breaking news appear on the home page', async function(assert) {
+    server.createList('breaking-news', 4);
+
+    await visit('/');
+
+    assert.dom('[data-test-breaking-news]').exists({count: 4});
+  });
+
+  test('external breaking news links open in new window', async function(assert) {
+    server.create('breaking-news', {
+      url: 'http://example.com'
+    });
+
+    await visit('/');
+
+    assert.dom('[data-test-breaking-news] a').hasAttribute('target','_blank');
+    assert.dom('[data-test-breaking-news] a').hasAttribute('rel','noopener');
+  });
+
+  test('local breaking news links open in same window', async function(assert) {
+    server.create('breaking-news', {
+      url: '/news/'
+    });
+
+    await visit('/');
+
+    assert.dom('[data-test-breaking-news] a').doesNotHaveAttribute('target');
   });
 });
