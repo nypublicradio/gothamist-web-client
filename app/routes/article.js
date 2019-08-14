fd7828fc02231c59ff016964717fff7e8b4af795
Brian Whitton <bwhitton@wnyc.org>
2019-08-14 19:12:12 -0400

    don't need to make a hash for these images anymore

--- app/routes/article.js
+++ app/routes/article.js
@@ -72,7 +72,7 @@ export default Route.extend({
       tags: article.displayTags,
       authors: article.authors,
       image: {
-        full: article.leadImage ? wagtailImageUrl([{id: article.leadImage.image}, 640]) : null,
+        full: article.leadImage ? wagtailImageUrl([article.leadImage.image, 640]) : null,
       },
     });
 
