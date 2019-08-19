2af301a3362e9956d6f6806e3d108fc576e7aa2f
Brian Whitton <me@brianwhitton.com>
2019-08-19 09:55:56 -0400

    upate queries for author detail page

--- tests/acceptance/author-detail-test.js
+++ tests/acceptance/author-detail-test.js
@@ -1,4 +1,4 @@
-import { module, skip /*test*/ } from 'qunit';
+import { module, test} from 'qunit';
 import { visit, currentURL, click, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
@@ -10,11 +10,25 @@ module('Acceptance | author detail', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  skip('visiting author-detail', async function(assert) {
-    server.createList('article', COUNT * 5, {author_nickname: 'Foo Bar'});
-    await visit('/author/Foo%20Bar');
+  const AUTHOR_SLUG = 'foo-bar';
 
-    assert.equal(currentURL(), '/author/Foo%20Bar');
+  hooks.beforeEach(() => {
+    // author page
+    server.create('page', {
+      html_path: 'staff/foo-bar/',
+      slug: AUTHOR_SLUG,
+      title: 'Foo Bar',
+    });
+    // articles by author are looked up by `author_slug` via REST query
+    server.createList('article', COUNT * 5, {
+      author_slug: AUTHOR_SLUG,
+    });
+  });
+
+  test('visiting author-detail', async function(assert) {
+    await visit(`/staff/${AUTHOR_SLUG}`);
+
+    assert.equal(currentURL(), '/staff/foo-bar');
 
     assert.dom('[data-test-block]').exists({count: COUNT});
     assert.dom('[data-test-author-heading]').hasText('Foo Bar');
@@ -24,17 +38,14 @@ module('Acceptance | author detail', function(hooks) {
     assert.dom('[data-test-block]').exists({count: COUNT * 2}, 'Clicking "read more" brings in another set of results equal to the amount of COUNT');
   });
 
-  skip('author lists get updated with commentCount', async function(assert) {
-    server.createList('article', COUNT * 5, {author_nickname: ['Foo']});
+  test('author lists get updated with commentCount', async function(assert) {
     const EXPECTED = server.schema.articles.all()
       .models.map((a, i) => ({posts: Math.ceil(Math.random() * i + 1), identifiers: [a.id]}));
 
     server.get(`${config.disqusAPI}/threads/set.json`, {response: EXPECTED});
-
-    await visit('/author/Foo');
+    await visit(`/staff/${AUTHOR_SLUG}`);
 
     await click('[data-test-more-results]');
-
 
     // assert that articles loaded via "read more" also get updated
     findAll('[data-test-block]').forEach(block => {
