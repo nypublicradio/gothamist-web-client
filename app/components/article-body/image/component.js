c009a76cd81bd47068b219f310ff4b0ea32215b2
Brian Whitton <bwhitton@wnyc.org>
2019-09-18 11:00:05 -0400

    derive height using this ratio calculation

--- app/components/article-body/image/component.js
+++ app/components/article-body/image/component.js
@@ -1,5 +1,16 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
+
+  width: 630,
+
+  height: computed('value.image', function() {
+    if (this.value.image) {
+      let { height, width } = this.value.image;
+      let newHeight = (this.width * height) / width;
+      return Math.round(newHeight);
+    }
+  }),
 });
