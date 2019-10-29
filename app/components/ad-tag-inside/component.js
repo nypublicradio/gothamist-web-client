e5b9b5a8817573fbc1b8027ef9d647a6ec4dd16c
walsh9 <walsh9@gmail.com>
2019-10-29 15:36:50 -0400

    don't worry about this case until we sort out ad other insertion issues

--- app/components/ad-tag-inside/component.js
+++ app/components/ad-tag-inside/component.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-// import { next } from '@ember/runloop';
 import { and, not } from '@ember/object/computed';
 
 export default Component.extend({
@@ -17,17 +16,7 @@ export default Component.extend({
 
   actions: {
     handleInsert(insertedTarget) {
-      // console.log('inserted wormhole target', insertedTarget)
       this.set('wormholeDestination', insertedTarget);
-      // if (this.ref && typeof htlbid !== "undefined") {
-      //   next(() => {
-      //     htlbid.forceRefresh(this.ref);
-      //   })
-      // }
-    },
-    handleSlotRenderEnded(/*event*/) {
-      // console.log('rendered ad', event)
-      // this.ref = event.ref;
-    },
+    }
   }
 });
