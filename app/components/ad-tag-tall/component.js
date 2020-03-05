a53e907ec2f440e1ac0fcaf24c6932cb7f555255
walsh9 <walsh9@gmail.com>
2020-03-05 13:24:40 -0500

    remove wrapper div for tall ad component

--- app/components/ad-tag-tall/component.js
+++ app/components/ad-tag-tall/component.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import config from 'gothamist-web-client/config/environment';
 
 export default Component.extend({
-  classNames: ['ad-tag-wide'],
+  tagName: '',
 
   isEager: (!config.lazyLoadAds),
 
