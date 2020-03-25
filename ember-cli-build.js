9920115ce4e4b01b50a95063d9cfb4ba9d4b5aae
Jeff Keen <jeff@keen.me>
2020-03-25 10:51:22 -0500

    Update git ignore, add pattern lab css to alternate stylesheet options, add missing classes

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -25,7 +25,7 @@ module.exports = function(defaults) {
       enabled: false,
     },
     'nypr-design-system': {
-      theme: 'gothamist'
+      themes: ['gothamist', 'white-label', 'deprecated']
     }
   });
 
