3166d3fcf11e779ca909dc660aa47a02917af022
walsh9 <walsh9@gmail.com>
2020-06-02 11:16:01 -0400

    Remove redundant config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,9 +26,6 @@ module.exports = function(defaults) {
       // JS preload fails due to a bug in chromium
       // patched, should land soon: https://chromium.googlesource.com/chromium/src.git/+/664b6639caeb2e0e7a9755db5a69256050b9d2e2
       enabled: false,
-    },
-    'nypr-design-system': {
-      themes: ['gothamist', 'white-label', 'deprecated']
     }
   });
 
