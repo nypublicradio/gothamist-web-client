fee28702b7ed947599f3ed9637b7288c88d191d0
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2020-06-11 12:57:55 -0400

    build ember app & run locally w/o AWS credentials + S3 fetch of bundle

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -16,6 +16,16 @@ wagtail)
     supervisord -c nginx/supervisord.conf
     ;;
 *)
+    if ! [[ -f ".env" ]]; then
+        cp .env.sample .env
+    fi
+    # AFAIK no good way to _only_ install devDependencies
+    # so we have to run `yarn install` again w/o the `-prod` flag
+    if ! [[ -d "dist" ]]; then
+        yarn install && yarn build
+    fi
+    export DIST_PATH="dist/"
+    export HOST="localhost"
     node fastboot
     ;;
 esac
