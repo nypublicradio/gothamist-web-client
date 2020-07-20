84440fafff8046e8319f26399d6d6a1e00ef0ec3
Kelly Suitor <ksuitor@nypublicradio.org>
2020-07-20 17:33:40 -0400

    Fixing Newsletter tests, added class

--- tests/acceptance/newsletter-test.js
+++ tests/acceptance/newsletter-test.js
@@ -1,14 +1,16 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | newsletter', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('visiting /newsletter', async function(assert) {
     await visit('/newsletter');
 
     assert.equal(currentURL(), '/newsletter');
-    assert.dom('.c-newsletter-form__concrete').exists(1);
+    assert.dom('.newsletter-page .c-newsletter-form__concrete').exists({count: 1});
   });
 });
