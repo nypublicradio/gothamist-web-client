82d3fbe401d48f4fc493540f0cc2023555e12ee7
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2020-08-26 01:03:58 -0400

    more test variables

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -11,6 +11,7 @@ wagtail)
     if ! [[ -f ".env" ]]; then
         cp .env.sample .env
         echo "TKTKTK HOST_WHITELIST = >$HOST_WHITELIST<"
+        echo "TKTKTK SQUASH_DOMAIN = >$SQUASH_DOMAIN<"
     fi
     # AFAIK no good way to _only_ install devDependencies
     # so we have to run `yarn install` again w/o the `-prod` flag
