86740a01a758750ab4c7a47c02156b28e30664ed
walsh9 <walsh9@gmail.com>
2021-01-12 12:52:58 -0500

    fix failing tests

--- tests/integration/modifiers/track-impression-test.js
+++ tests/integration/modifiers/track-impression-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Modifier | track-impression', function(hooks) {
   setupRenderingTest(hooks);
