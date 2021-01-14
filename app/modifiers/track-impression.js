dcf870543a2d71b24896ce7175a80cc87174b1d0
walsh9 <walsh9@gmail.com>
2021-01-14 16:57:48 -0500

    Update newsletter tracking

--- app/modifiers/track-impression.js
+++ app/modifiers/track-impression.js
@@ -4,8 +4,9 @@ export default modifier(function trackImpression(element/*, params, hash*/) {
   let eventCategory = element.dataset.category;
   let eventAction = element.dataset.action;
   let eventLabel = element.dataset.label;
+
   function trackImpression() {
-    if (window && window.dataLayer && !element.dataset.viewed) {
+    if (window.dataLayer && !element.dataset.viewed) {
       window.dataLayer.push({
         event: "impression",
         eventCategory,
@@ -16,21 +17,27 @@ export default modifier(function trackImpression(element/*, params, hash*/) {
       element.dataset.viewed = true;
     }
   }
-  function onChange(changes, observer) {
+  function elementIntersectionChanged(changes/*, observer*/) {
     changes.forEach(change => {
-      if (change.intersectionRatio >= 1) {
+      if (change.intersectionRatio >= 0.75) {
         trackImpression();
-        observer.disconnect();
       }
     });
   }
-  if (window && 'IntersectionObserver' in window) {
+  function routeChanged() {
+    // reset impression when route changes
+    delete element.dataset.viewed;
+  }
+
+  window.addEventListener('routeChange', routeChanged);
+
+  if ('IntersectionObserver' in window) {
     let options = {
         root: null, //viewport
         rootMargin: '0px',
-        threshold: 1.0
+        threshold: 0.75
     };
-    let observer = new IntersectionObserver(onChange, options);
+    let observer = new IntersectionObserver(elementIntersectionChanged, options);
     observer.observe(element);
 
     return () => observer.disconnect();
