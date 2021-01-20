6cd013de6ad45533684dc19761cdf0ab02dd5a76
walsh9 <walsh9@gmail.com>
2021-01-19 19:17:34 -0500

    Update event tracking test

--- tests/unit/utils/track-event-test.js
+++ tests/unit/utils/track-event-test.js
@@ -2,11 +2,10 @@ import { module, test } from 'qunit';
 
 import trackEvent from 'gothamist-web-client/utils/track-event';
 
-module('Unit | Utility | wagtail-api', function() {
+module('Unit | Utility | track-event', function() {
   test('it logs an event to the dataLayer', function(assert) {
     window.dataLayer = window.dataLayer || [];
     let eventData = {
-      event: "test-event",
       category: "Test Category",
       action: "Test Action",
       label: "Test Label"
@@ -15,7 +14,7 @@ module('Unit | Utility | wagtail-api', function() {
     trackEvent(eventData);
 
     const data = window.dataLayer.pop();
-    assert.equal(data['event'], "test-event");
+    assert.equal(data['event'], "eventTracking");
     assert.equal(data['eventCategory'], "Test Category");
     assert.equal(data['eventAction'], "Test Action");
     assert.equal(data['eventLabel'], "Test Label");
