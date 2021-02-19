b16479d427e99b1cde4ab65864bbe42d98666651
walsh9 <walsh9@gmail.com>
2021-02-19 16:50:04 -0500

    Add tests for curated tag pages

--- tests/acceptance/tags-test.js
+++ tests/acceptance/tags-test.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 import { module, test } from 'qunit';
-import { visit, currentURL, click,findAll } from '@ember/test-helpers';
+import { visit, currentURL, click, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -80,4 +80,84 @@ module('Acceptance | tags', function(hooks) {
     assert.dom('[data-test-top-product-banner] .o-box-banner__dek').includesText("Test Description");
     assert.dom('[data-test-top-product-banner] .o-box-banner__cta').includesText("Test Button");
   });
+
+  test('tag page customization works', async function(assert) {
+    const tagPage = server.create('tagpage', {slug: 'dogs-and-cats'})
+    server.createList('article', COUNT * 5, {tags: [{slug: 'dogs-and-cats', name: 'dogs and cats'}], section: 'food', tag_slug: 'dogs-and-cats'});
+    await visit('/tags/dogs-and-cats');
+
+    const headerImage =  tagPage.designed_header[0].value.image.id
+    const headerImageUrl = `https://example.com/images/${headerImage}/fill-1600x277/`
+    const topPageText = tagPage.top_page_zone[0].value.split('</p')[0].replace('<p>','')
+    const midpageText = tagPage.midpage_zone[0].value.split('</p')[0].replace('<p>','')
+
+    assert.dom('[data-test-designed-header] > img').hasAttribute('src', headerImageUrl)
+    assert.dom('[data-test-top-page-zone]').includesText(topPageText)
+    assert.dom('[data-test-midpage-zone]').includesText(midpageText)
+  });
+
+  test('curated content appears in top page zone', async function(assert) {
+    const tagPage = server.create('tagpage', {slug: 'dogs-and-cats'}, 'hasCollectionInTopPageZone')
+    server.createList('article', COUNT * 5, {tags: [{slug: 'dogs-and-cats', name: 'dogs and cats'}], section: 'food', tag_slug: 'dogs-and-cats'});
+
+    const articles = tagPage.top_page_zone[0].value.pages
+
+    await visit('/tags/dogs-and-cats');
+
+    const cards = findAll('[data-test-top-page-zone] [data-test-block]')
+    assert.equal(cards.length, 2, 'top page zone should contain two article cards')
+
+    cards.forEach((card, index) => {
+      assert.dom('[data-test-block-title]', card).hasText(articles[index].title, `card ${index} should show the correct title`)
+    })
+  });
+
+
+  test('curated content appears in midpage zone', async function(assert) {
+    const tagPage = server.create('tagpage', {slug: 'dogs-and-cats'}, 'hasCollectionInMidpageZone')
+    server.createList('article', COUNT * 5, {tags: [{slug: 'dogs-and-cats', name: 'dogs and cats'}], section: 'food', tag_slug: 'dogs-and-cats'});
+
+    const articles = tagPage.midpage_zone[0].value.pages
+
+    await visit('/tags/dogs-and-cats');
+
+    const cards = findAll('[data-test-midpage-zone] [data-test-block]')
+    assert.equal(cards.length, 2, 'midpage zone should contain two article cards')
+
+    cards.forEach((card, index) => {
+      assert.dom('[data-test-block-title]', card).hasText(articles[index].title, `card ${index} should show the correct title`)
+    })
+  });
+
+  test('curated content shows comment counts', async function(assert) {
+    const tagPage = server.create('tagpage', {slug: 'dogs-and-cats'}, 'hasCollectionInTopPageZone', 'hasCollectionInMidpageZone')
+    server.createList('article', COUNT * 5, {tags: [{slug: 'dogs-and-cats', name: 'dogs and cats'}], section: 'food', tag_slug: 'dogs-and-cats'});
+
+
+    // fake disqus API
+    const COMMENT_DATA = server.schema.articles.all()
+      .models.map((a, i) => ({posts: Math.ceil(Math.random() * i + 1), identifiers: [a.uuid]}));
+    server.get(`${config.disqusAPI}/threads/set.json`, (schema, request) => {
+        let ids =  request.url.split('&').filter(p => p.startsWith('thread:ident=')).map(p => p.replace('thread:ident=', ''));
+        let filteredResponse = EXPECTED.filter(item => ids.includes(item.identifiers[0]))
+        return {response: filteredResponse}
+    });
+
+    const topArticles = tagPage.top_page_zone[0].value.pages
+    const midArticles = tagPage.midpage_zone[0].value.pages
+    const allArticles = topArticles.concat(midArticles)
+
+    await visit('/tags/dogs-and-cats');
+
+    const topCards = findAll('[data-test-top-page-zone] [data-test-block]')
+    const midCards = findAll('[data-test-midpage-zone] [data-test-block]')
+    const allCards = topCards.concat(midCards)
+
+    allCards.forEach((card, index) => {
+      let commentCount = COMMENT_DATA.find(d => d.identifiers.includes(allArticles[index].id))
+      assert.dom('.c-block-meta__comments', card).exists(`card ${index} should have comments`)
+      assert.dom('.c-block-meta__comments', card).includesText(String(commentCount), `card ${index} should show the correct comment count`)
+    })
+  });
+
 });
