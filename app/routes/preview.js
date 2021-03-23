3929caf5443668b346e7ce287739f593b7d088ae
Michael Hearn <git@mikehearn.net>
2021-03-22 21:41:34 -0400

    Add tag model render logic to preview route

--- app/routes/preview.js
+++ app/routes/preview.js
@@ -1,9 +1,11 @@
+import DS from 'ember-data';
 import Route from '@ember/routing/route';
 import { get } from '@ember/object';
 import { inject } from '@ember/service';
 import { WAGTAIL_MODEL_TYPE as ARTICLE_TYPE } from '../models/article';
 import { WAGTAIL_MODEL_TYPE as GALLERY_TYPE } from '../models/gallery';
 import { WAGTAIL_MODEL_TYPE as INFORMATION_TYPE } from '../models/information';
+import { WAGTAIL_MODEL_TYPE as TAG_TYPE } from '../models/tag';
 import RSVP from 'rsvp';
 const { hash, resolve } = RSVP;
 const { log } = console;
@@ -40,6 +42,40 @@ export default Route.extend({
         }).then(model => {
           this.render('gallery', { model })
         });
+        break;
+      case TAG_TYPE:
+        this.controllerFor('page').set('isPreview', true);
+        hash({
+          tag: model.get('slug'),
+          title: model.get('slug').replace(/-/g, ' '),
+          page: model,
+          articles: this.store.query('article', {
+            tag_slug: model.get('slug'),
+            limit: 12,
+          })
+        }).then(results => {
+          if (results.articles.length === 0) {
+            let e = new DS.NotFoundError();
+            e.url = `tags/${results.tag}`;
+            throw e;
+          }
+          // get tag name from first article
+          results.title = results.articles.firstObject.tags.findBy('slug', model.get('slug'))['name']
+          return results;
+        }).then(model => {
+          this.render('tags', { model })
+        });
+
+
+
+
+
+
+        // hash({
+        //   tag: model
+        // }).then(model => {
+        //   this.render('tags', { model })
+        // });
         break;
       case INFORMATION_TYPE:
         this.controllerFor('information').set('isPreview', true);
